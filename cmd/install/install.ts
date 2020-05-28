37c5091e013bd56acb398ed21879fdb3e0465f35
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-05-27 22:50:29 -0300

    New changes

--- cmd/install/install.ts
+++ cmd/install/install.ts
@@ -1,10 +1,11 @@
 import Command from "../../lib/command.ts";
+import Config from "../../lib/config.ts";
 
 export default class Install implements Command {
-    constructor() {
+    constructor(private config:Config) {
     }
 
     execute(args: string[]): void {
-        console.log("install " + JSON.stringify(args));
+        console.log("install " + JSON.stringify(args) + " config=" + JSON.stringify(this.config.repository));
     }
 }
