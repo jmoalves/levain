64b10fce6abc030e55754aadca650167c935b53d
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-05-27 22:56:58 -0300

    Package resolving

--- cmd/install/install.ts
+++ cmd/install/install.ts
@@ -1,5 +1,7 @@
 import Command from "../../lib/command.ts";
 import Config from "../../lib/config.ts";
+import Package from "../../lib/package.ts";
+import Repository from "../../lib/repository.ts";
 
 export default class Install implements Command {
     constructor(private config:Config) {
@@ -7,5 +9,58 @@ export default class Install implements Command {
 
     execute(args: string[]): void {
         console.log("install " + JSON.stringify(args) + " config=" + JSON.stringify(this.config.repository));
+
+        let error:boolean = false;
+        let pkgs:Map<string, Package> = new Map();
+        for (const pkgName of args) {
+            let myError:boolean = this.resolvePackages(this.config.repository, pkgs, pkgName);
+            error = error || myError;
+        }
+
+        if (error) {
+            console.error("");
+            return;
+        }
+
+        console.log("");
+        console.log("================");
+        for (let name of pkgs.keys()) {
+            let pkg = pkgs.get(name);
+            if (pkg) {
+                this.installPackage(pkg);
+            }
+        }
     }
+
+    private resolvePackages(repo:Repository, pkgs:Map<string, Package>, pkgName:string):boolean {
+        if (pkgs.has(pkgName)) {
+            return false;
+        }
+
+        const pkgDef = repo.resolvePackage(pkgName);
+        if (!pkgDef) {
+            console.error("PACKAGE NOT FOUND: " + pkgName);
+            return true;
+        }    
+
+        // Busca dependências primeira - Ordenação topológica por busca em profundidade
+        let error:boolean = false;
+        if (pkgDef.dependencies) {
+            for (let dep of pkgDef.dependencies) {
+            let myError:boolean = this.resolvePackages(repo, pkgs, dep);
+            error = error || myError;
+            }
+        }
+
+        pkgs.set(pkgName, pkgDef);
+        return error;
+    }
+
+    private installPackage(pkg: Package): void {
+        if (!this.config) {
+            return;
+        }
+
+        console.log("INSTALL " + pkg.name);
+    }    
 }
