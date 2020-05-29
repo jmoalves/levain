f92275583a8c81ba03e53f08ee3a580fff3cf6c3
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-05-29 16:28:15 -0300

    Handling cmd line

--- levain.ts
+++ levain.ts
@@ -6,31 +6,33 @@ const { args, exit } = Deno;
 console.log("levain v0.0.1");
 console.log("");
 
+// TODO: We should use a command line parser. But not Deno.flags.
 // args clone
 const myArgs:string[] = [];
+let optionSpace:boolean = true;
 for (let arg of args) {
-    myArgs.push(arg);    
+    if (optionSpace && arg.startsWith("-")) {
+        console.log("General option ignored", arg);
+    } else {
+        optionSpace = false;
+        myArgs.push(arg);
+    }
 }
 
 // TODO: Handle general options
+
+// TODO: No parameters? Show Help
 if (myArgs.length == 0) {
-    // TODO: Show Help
     exit(1);
 }
 
-// First parameter is the command
-let first = myArgs.shift();
-if (first && first.startsWith("-")) { // TODO: Improve this!
-    console.error(`ERROR: Invalid levain option '${first}'`);
-    exit(1);
-}
-
-let cmd:string = first!;
-
+// Context
 const config = new Config();
-const loader = new Loader(config)
+//
 
 console.log("");
 console.log("==================================");
+// First parameter is the command
+let cmd:string = myArgs.shift()!;
+const loader = new Loader(config);
 loader.execute(cmd, myArgs);
-
