08630e669163663241c7fcce35187530bbec0c59
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-05-29 22:29:56 -0300

    removing bundle

--- levain.cmd
+++ levain.cmd
@@ -2,5 +2,5 @@
 cls 
 deno run ^
     --allow-read --allow-env --allow-net ^
-    dist/levain.v0.0.1.js ^
+    src/levain.ts ^
     %*
--- scripts/bundle.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-set version=0.0.1
-deno bundle src/levain.ts dist/levain.v%version%.js
