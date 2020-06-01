3557ba005884a7f3ab9f3da237e6eec732c5dc0b
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-05-31 22:52:26 -0300

    extract action inception

--- src/action/extract.ts
+++ src/action/extract.ts
@@ -1,3 +1,6 @@
+import { parse } from "https://deno.land/std/flags/mod.ts";
+import * as path from "https://deno.land/std/path/mod.ts";
+
 import Action from "../lib/action.ts";
 import Config from "../lib/config.ts";
 import Package from '../lib/package.ts';
@@ -7,6 +10,34 @@ export default class Extract implements Action {
     }
 
     execute(pkg:Package, parameters:string[]):void {
-        console.log("Extract: ", JSON.stringify(parameters));
+        let args = this.parseArgs(parameters);
+        
+        if (args._.length != 2) {
+            console.error("You must inform the file to extract and the destination directory");
+            throw "Illegal arguments";
+        }
+
+        const src = path.resolve(pkg.pkgDir, args._[0]);
+        const dst = path.resolve(pkg.baseDir, args._[1]);
+
+        console.log("EXTRACT", src, "=>", dst);
+    }
+
+    private parseArgs(args: string[]): any {
+        return parse(args, {
+            string: [
+            ],
+            boolean: [
+            ],
+            stopEarly: true,
+            unknown: (v) => { 
+                if (v.startsWith("-")) {
+                    console.log("ERROR: Unknown option", v);
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        });    
     }
 }
