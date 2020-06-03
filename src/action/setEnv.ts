758bca4d4e88be7c09d31b880538cb948dd7aff6
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-06-03 17:06:28 -0300

    setEnv

---
+++ src/action/setEnv.ts
@@ -0,0 +1,32 @@
+import * as path from "https://deno.land/std/path/mod.ts";
+
+import Action from "../lib/action.ts";
+import Config from "../lib/config.ts";
+import Package from '../lib/package/package.ts';
+
+export default class SetEnv implements Action {
+    constructor(private config:Config) {
+    }
+
+    execute(context: any, pkg:Package, parameters:string[]): void {
+        if (parameters.length != 2) {
+            throw "You must inform the var and the value";
+        }
+
+        if (!context.action) {
+            context.action = {};
+        }
+
+        if (!context.action.setEnv) {
+            context.action.setEnv = {};
+        }
+
+        if (!context.action.setEnv.path) {
+            context.action.setEnv.env = {};
+        }
+
+        let key = parameters[0];
+        let value = parameters[1];
+        context.action.setEnv.env[key] = value;
+    }
+}
