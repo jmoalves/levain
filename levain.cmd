0a1f708ad9b17982eafdfd3e85a566ca9449f60b
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-06-03 14:56:05 -0300

    Deno v1.0.5

--- levain.cmd
+++ levain.cmd
@@ -1,6 +1,7 @@
 @echo off
 
 set myPath=%~dp0
+%myPath%bin\deno.exe -V 
 %myPath%bin\deno.exe run ^
     --allow-read --allow-write --allow-env --allow-net --allow-run ^
     --unstable ^
--- levain.sh
+++ levain.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 myPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+$myPath/bin/deno -V
 $myPath/bin/deno run \
     --allow-read --allow-write --allow-env --allow-net --allow-run \
     --unstable \
