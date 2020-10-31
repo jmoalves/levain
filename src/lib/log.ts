5dc6057bf1f742575f83c3090a11b2fe6fa35706
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-10-31 15:44:55 -0300

    using std-log instead of console.log - Start

---
+++ src/lib/log.ts
@@ -0,0 +1,45 @@
+import * as log from "https://deno.land/std/log/mod.ts";
+
+import { homedir } from './utils.ts';
+
+export default class Logger {
+    public static async setup() {
+        await log.setup({
+            handlers: {
+              console: new log.handlers.ConsoleHandler("INFO"),
+          
+              file: new log.handlers.FileHandler("INFO", {
+                filename: `${homedir()}/levain-${Logger.logTag(new Date())} .log`,
+                formatter: logRecord => {
+                    return `${Logger.logTag(logRecord.datetime)} ${logRecord.levelName} ${logRecord.msg}`;
+                  }
+              }),
+            },
+          
+            loggers: {
+              // configure default logger available via short-hand methods above
+              default: {
+                level: "INFO",
+                handlers: ["console", "file"],
+              },
+          
+              engine: {
+                level: "ERROR",
+                handlers: ["console"],
+              },
+            },
+          });
+    }
+
+    private static logTag(dt:Date): string {
+        let logTag: string = "";
+        logTag += dt.getFullYear() + "";
+        logTag += (dt.getMonth() < 10 ? "0" : "") + dt.getMonth();
+        logTag += (dt.getDate() < 10 ? "0" : "") + dt.getDate();
+        logTag += "-";
+        logTag += (dt.getHours() < 10 ? "0" : "") + dt.getHours();
+        logTag += (dt.getMinutes() < 10 ? "0" : "") + dt.getMinutes();
+        logTag += (dt.getSeconds() < 10 ? "0" : "") + dt.getSeconds();
+        return logTag;
+    }
+}
