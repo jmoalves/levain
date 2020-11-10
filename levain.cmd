11543653ee1995d18aed8b0bc0a0a3bec7d8ab1c
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-10 13:15:09 -0300

    Assume deno.exe in PATH

--- levain.cmd
+++ levain.cmd
@@ -4,7 +4,7 @@ set myPath=%~dp0
 
 set NO_COLOR=true
 
-%myPath%bin\deno.exe run ^
+deno.exe run ^
     --allow-read --allow-write --allow-env --allow-net --allow-run ^
     --unstable ^
     %myPath%src/levain.ts ^
--- levain.sh
+++ levain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 myPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$myPath/bin/deno run \
+deno run \
     --allow-read --allow-write --allow-env --allow-net --allow-run \
     --unstable \
     $myPath/src/levain.ts \
