841ee1289361c99da6e5b98ddbbeb1ee3ee5db31
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-15 15:52:58 -0300

    Using import.meta.main as advised at https://deno.land/manual/tools/script_installer

--- src/levain.ts
+++ src/levain.ts
@@ -5,45 +5,52 @@ import Loader from './lib/loader.ts';
 import Config from './lib/config.ts';
 import { parseArgs } from "./lib/parseArgs.ts";
 
-await Logger.setup();
+async function levainCLI(): Promise<void> {
+    await Logger.setup();
 
-log.info(`  deno v${Deno.version.deno}`);
-log.info(`levain vHEAD`);
-log.info("");
+    log.info(`  deno v${Deno.version.deno}`);
+    log.info(`levain vHEAD`);
+    log.info("");
 
-const myArgs = parseArgs(Deno.args, {
-    stringOnce: [
-        "levainHome"
-    ],
-    stringMany: [
-        "addRepo"
-    ],
-    boolean: [
-    ]
-});
-log.debug("args " + JSON.stringify(myArgs));
+    const myArgs = parseArgs(Deno.args, {
+        stringOnce: [
+            "levainHome"
+        ],
+        stringMany: [
+            "addRepo"
+        ],
+        boolean: [
+        ]
+    });
+    log.debug("args " + JSON.stringify(myArgs));
 
-// Context
-const config = new Config(myArgs);
-//
+    // Context
+    const config = new Config(myArgs);
+    //
 
-// TODO: No parameters? Show Help
-if (myArgs._.length == 0) {
-    log.error("");
-    log.error("Nothing to do. Do you want some help?");
-    Deno.exit(1);
+    // TODO: No parameters? Show Help
+    if (myArgs._.length == 0) {
+        log.error("");
+        log.error("Nothing to do. Do you want some help?");
+        Deno.exit(1);
+    }
+
+    log.info("");
+    log.info("==================================");
+
+    // First parameter is the command
+    let cmd:string = myArgs._.shift()!;
+    const loader = new Loader(config);
+    await loader.command(cmd, myArgs._);
+
+    /////////////////////////////////////////////////////////////////////////////////
+    log.info("");
+    log.info("==================================");
+    log.info("Levain fermentee!");
+    log.info("");
 }
 
-log.info("");
-log.info("==================================");
-
-// First parameter is the command
-let cmd:string = myArgs._.shift()!;
-const loader = new Loader(config);
-await loader.command(cmd, myArgs._);
-
-/////////////////////////////////////////////////////////////////////////////////
-log.info("");
-log.info("==================================");
-log.info("Levain fermentee!");
-log.info("");
+// https://deno.land/manual/tools/script_installer
+if (import.meta.main) {
+    levainCLI();
+}
