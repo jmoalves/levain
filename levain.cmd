6ece1b4b46de19b5d6c878f925b45e4fc087f40d
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-15 19:46:48 -0300

    levain install - define PATH for levain itself

--- levain.cmd
+++ levain.cmd
@@ -2,9 +2,12 @@
 
 set myPath=%~dp0
 
+set denoPath=%myPath%bin\
+if "a%myPath" == "a" set denoPath=
+
 set NO_COLOR=true
-%myPath%bin\deno.exe run ^
+%denoPath%deno.exe run ^
     --allow-read --allow-write --allow-env --allow-net --allow-run ^
     --unstable ^
-    %myPath%src/levain.ts ^
+    %myPath%src\levain.ts ^
     %*
