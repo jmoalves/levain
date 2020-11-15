3d200b11b3e81baec076950acc0ca0e972fdfa69
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-15 18:49:21 -0300

    Not changing PATH

--- levain.cmd
+++ levain.cmd
@@ -1,11 +1,9 @@
 @echo off
 
 set myPath=%~dp0
-PATH %myPath%;%myPath%bin;%PATH%
 
 set NO_COLOR=true
-
-deno.exe run ^
+%myPath%bin\deno.exe run ^
     --allow-read --allow-write --allow-env --allow-net --allow-run ^
     --unstable ^
     %myPath%src/levain.ts ^
