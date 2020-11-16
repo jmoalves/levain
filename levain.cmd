b492252557cea1e98b8a82ae1a6519607c1cd94c
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-16 19:01:07 -0300

    deno with cached-only

--- levain.cmd
+++ levain.cmd
@@ -8,6 +8,7 @@ if "a%myPath" == "a" set denoPath=
 set NO_COLOR=true
 set DENO_DIR=%denoPath%
 %denoPath%deno.exe run ^
+    --cached-only ^
     --allow-read --allow-write --allow-env --allow-net --allow-run ^
     --unstable ^
     %myPath%src\levain.ts ^
--- levain.sh
+++ levain.sh
@@ -2,7 +2,9 @@
 
 myPath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+set DENO_DIR=$myPath/bin
 $myPath/bin/deno run \
+    --cached-only \
     --allow-read --allow-write --allow-env --allow-net --allow-run \
     --unstable \
     $myPath/src/levain.ts \
