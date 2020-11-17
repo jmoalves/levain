af6b229d55d07ccfbdfd0de895917a578c8d8ab4
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-17 11:07:37 -0300

    Function to prompt for passwords

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,3 +22,44 @@ export var homedir = function(): string {
     // What else?
     throw "No home for levain. Do you have a refrigerator?";
 }
+
+// https://github.com/caspervonb/deno-prompts/blob/master/mod.ts
+export async function promptSecret(message : string) : Promise<string | null> {
+	Deno.stdout.write(new TextEncoder().encode(message));
+	Deno.setRaw(0, true);
+
+	let input = "";
+	while (true) {
+		const data = new Uint8Array(1);
+		const nread = await Deno.stdin.read(data);
+		if (!nread) {
+			break;
+		}
+
+		const string = new TextDecoder().decode(data.slice(0, nread));
+
+		for (const char of string) {
+			switch (char) {
+				case "\u0003":
+				case "\u0004":
+					Deno.setRaw(Deno.stdin.rid, false);
+					return null;
+
+				case "\r":
+				case "\n":
+					Deno.setRaw(Deno.stdin.rid, false);
+					return input;
+
+				case "\u0008":
+					input = input.slice(0, input.length - 1);
+					break;
+
+				default:
+					input += char;
+					break;
+			}
+		}
+	}
+
+	return null;
+}
