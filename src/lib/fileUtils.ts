66c156f07e4a288af0a3b854bb64b2fc2e761591
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-18 11:35:03 -0300

    Using log

--- src/lib/fileUtils.ts
+++ src/lib/fileUtils.ts
@@ -1,9 +1,11 @@
+import * as log from "https://deno.land/std/log/mod.ts";
+
 export class FileUtils {
 
     static getModificationTimestamp(filePath: string): Date|undefined {
         const stat = Deno.statSync(filePath);
         const modificationTimestamp = stat.mtime;
-        console.info('getModificationTimestamp', modificationTimestamp, filePath);
+        log.info(`getModificationTimestamp ${modificationTimestamp} --> ${filePath}`);
         return modificationTimestamp || undefined;
     }
 
