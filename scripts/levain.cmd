930e8ce8cc43c275864a2325b8dd389f5d7d9d43
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-18 18:23:22 -0300

    scripts - levain.cmd - removing --reload

--- scripts/levain.cmd
+++ scripts/levain.cmd
@@ -3,7 +3,7 @@
 set myPath=%~dp0
 
 set NO_COLOR=true
-deno.exe run --reload ^
+deno.exe run ^
     --allow-read --allow-write --allow-env --allow-net --allow-run ^
     --unstable ^
     %myPath%..\src\levain.ts ^
