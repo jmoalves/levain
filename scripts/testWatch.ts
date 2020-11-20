0524fa699859f10338b7bd1417ab862fb26926c1
rafaelwalter <rafawalter@gmail.com>
2020-11-19 22:38:09 -0300

    testWatch: run all testes first

--- scripts/testWatch.ts
+++ scripts/testWatch.ts
@@ -1,12 +1,23 @@
 const watcher = Deno.watchFs('./src/')
 
+runTest()
+
 for await (const event of watcher) {
     // console.log('>>>> event', event);
     if (['create', 'modify'].indexOf(event.kind)) {
         event.paths.forEach((file) => {
-            if (file.endsWith('.ts')) {
-                Deno.run({ cmd: ['deno', 'test', '--unstable', '--allow-env']})
+            if (file.endsWith('.ts') && (!file.indexOf('$deno$'))) {
+                runTest(file)
             }
         })
     }
+}
+
+function runTest(file?: string): void {
+    let cmd = ['deno', 'test', '--unstable', '--allow-env'];
+    console.log('runTest', file)
+    if (file) {
+        cmd.push(file)
+    }
+    Deno.run({cmd})
 }
