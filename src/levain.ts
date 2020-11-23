7614bf8fad0f441a6176c85a8339c50be91a3221
rafaelwalter <rafawalter@gmail.com>
2020-11-23 15:38:51 -0300

    v0.1.7

--- src/levain.ts
+++ src/levain.ts
@@ -10,7 +10,7 @@ export async function levainCLI(): Promise<void> {
     await ConsoleAndFileLogger.setup();
 
     log.info(`  deno v${Deno.version.deno}`);
-    log.info(`levain vHEAD`);
+    log.info(`levain v0.1.7`);
 
     const myArgs = parseArgs(Deno.args, {
         stringOnce: [
