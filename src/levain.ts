d339c8e08589330d637a5af0a3fd44e6074ef0bf
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-24 13:51:08 -0300

    v0.2.3

--- src/levain.ts
+++ src/levain.ts
@@ -8,7 +8,7 @@ import {askPassword, askUsername} from "./lib/credentials.ts";
 
 export async function levainCLI(): Promise<void> {
     log.info(`  deno v${Deno.version.deno}`);
-    log.info(`levain vHEAD`);
+    log.info(`levain v0.2.3`);
 
     const myArgs = parseArgs(Deno.args, {
         stringOnce: [
