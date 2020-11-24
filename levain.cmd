cc9343e8c170f7dd34393fb36c2dde0362918fb5
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-24 07:34:27 -0300

    levain.cmd - handle cached-only option

--- levain.cmd
+++ levain.cmd
@@ -3,15 +3,20 @@
 set myPath=%~dp0
 
 set denoPath=%myPath%bin\
-if "a%myPath" == "a" set denoPath=
-if not "a%denoPath" == "a" (
+if "a%myPath%" == "a" set denoPath=
+if not "a%denoPath%" == "a" (
     if not exist %denoPath%deno.exe set denoPath=
+)
+
+set cachedOption=
+if not "a%denoPath%" == "a" (
+    set cachedOption=--cached-only
 )
 
 set NO_COLOR=true
 set DENO_DIR=%denoPath%
 %denoPath%deno.exe run ^
-    --cached-only ^
+    %cachedOption% ^
     --allow-read --allow-write --allow-env --allow-net --allow-run ^
     --unstable ^
     %myPath%src\levain.ts ^
