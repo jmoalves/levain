6a1f914bd62a9ead32719adb9ccd67eefab5c12a
Rafael Walter <rafawalter@gmail.com>
2020-11-25 22:08:42 -0300

    crawl Windows networks

--- src/lib/repository/file_system_repository.ts
+++ src/lib/repository/file_system_repository.ts
@@ -7,12 +7,34 @@ import Repository from './repository.ts'
 import FileSystemPackage from '../package/file_system_package.ts'
 import Config from '../config.ts';
 import {Timer} from "../timer.ts";
+import {OsShell} from '../shellUtils.ts';
 
 export default class FileSystemRepository implements Repository {
     readonly excludeDirs = ['$RECYCLE.BIN', 'node_modules', '.git']
 
     constructor(private config: Config, private rootDir: string) {
         log.debug(`FSRepo: Root=${this.rootDir}`);
+    }
+
+
+    readonly name = `fileSystemRepo for ${this.rootDir}`;
+
+    resolvePackage(packageName: string): FileSystemPackage | undefined {
+        if (!existsSync(`${this.rootDir}`)) {
+            return undefined;
+        }
+
+        const pkg = this.readPackageFromList(packageName);
+        if (pkg) {
+            log.debug(`FSRepo: ${packageName} => ${pkg.toString()}`);
+        }
+
+        return pkg;
+    }
+
+    private readPackageFromList(packageName: string): FileSystemPackage | undefined {
+        return this.packages
+            .find(pkg => pkg.name == packageName);
     }
 
     _packages: Array<FileSystemPackage> | undefined;
@@ -22,71 +44,6 @@ export default class FileSystemRepository implements Repository {
         }
         return this._packages;
     }
-
-    readonly name = `fileSystemRepo for ${this.rootDir}`;
-
-    resolvePackage(packageName: string): FileSystemPackage | undefined {
-        if (!existsSync(`${this.rootDir}`)) {
-            return undefined;
-        }
-
-        // let pkg = this.readPackageInDir(packageName, this.rootDir);
-        const pkg = this.readPackageFromList(packageName);
-        if (pkg) {
-            log.debug(`FSRepo: ${packageName} => ${pkg.toString()}`);
-        }
-
-        return pkg;
-    }
-
-    private readPackageInDir(packageName: string, dirname: string): FileSystemPackage | undefined {
-        let pkg: FileSystemPackage | undefined = undefined;
-        log.debug(`readDir ${packageName} ${dirname}`)
-        if (this.excludeDirs.find(ignoreDir => dirname.endsWith(ignoreDir))) {
-            log.debug(`ignoring ${dirname}`)
-            return
-        }
-
-        for (const entry of Deno.readDirSync(dirname)) {
-            if (!pkg && entry.isDirectory) {
-                pkg = this.readPackageInDir(packageName, path.resolve(dirname, entry.name));
-            }
-
-            if (!pkg && entry.isFile) {
-                pkg = this.readPackage(packageName, path.resolve(dirname, entry.name));
-            }
-
-            if (pkg) {
-                return pkg;
-            }
-        }
-
-        return undefined;
-    }
-
-    private readPackage(packageName: string, yamlFile: string): FileSystemPackage | undefined {
-        if (!path.basename(yamlFile).match("^" + packageName + ".levain.ya?ml$")) {
-            return undefined;
-        }
-
-        let fileinfo = Deno.lstatSync(yamlFile);
-        if (!fileinfo.isFile) {
-            return undefined;
-        }
-
-        let yamlStr: string = Deno.readTextFileSync(yamlFile);
-
-        let pkg: FileSystemPackage = new FileSystemPackage(
-            this.config,
-            packageName,
-            this.config.replaceVars(`\${levainHome}/${packageName}`),
-            yamlFile,
-            yamlStr,
-            this);
-
-        return pkg;
-    }
-
 
     listPackages(rootDirOnly?: boolean): Array<FileSystemPackage> {
         if (!existsSync(`${this.rootDir}`)) {
@@ -106,24 +63,85 @@ export default class FileSystemRepository implements Repository {
             exclude: this.excludeDirs,
         }
         log.debug(`# listPackages: ${packagesGlob} ${JSON.stringify(globOptions)}`)
-        const packageFiles = expandGlobSync(packagesGlob, globOptions)
-        const packages: Array<FileSystemPackage> = [];
-        for (const file of packageFiles) {
-            log.debug(`## checking file ${JSON.stringify(file)}`)
-            const packageName = file.name.replace(/\.levain\.ya?ml/, '')
-            const pkg = this.readPackage(packageName, file.path)
-            if (pkg) {
-                log.debug(`## adding package ${pkg}`)
-                packages.push(pkg)
-            }
-        }
+        const packages: Array<FileSystemPackage> = this.getPackageFiles(packagesGlob, globOptions)
+
         log.info(`added ${packages.length} packages in ${timer.humanize()}`)
         log.info("")
         return packages;
     }
 
-    private readPackageFromList(packageName: string): FileSystemPackage | undefined {
-        return this.packages
-            .find(pkg => pkg.name == packageName);
+    private getPackageFiles(packagesGlob: string, globOptions: ExpandGlobOptions): Array<FileSystemPackage> {
+        // FIXME Why, oh my...
+        if (OsShell.isWindows()) {
+            return this.crawlPackages(globOptions['root'] || '.', globOptions)
+        } else {
+            return this.globPackages(packagesGlob, globOptions);
+        }
     }
+
+    crawlPackages(dirname: string, options: ExpandGlobOptions): Array<FileSystemPackage> {
+        let packages: Array<FileSystemPackage> = [];
+        if (this.excludeDirs.find(ignoreDir => dirname.endsWith(ignoreDir))) {
+            log.debug(`ignoring ${dirname}`)
+            return packages;
+        }
+
+        log.debug(`crawlPackages ${dirname}`)
+        for (const entry of Deno.readDirSync(dirname)) {
+            if (entry.isDirectory) {
+                packages.concat(this.crawlPackages(path.resolve(dirname, entry.name), options));
+            }
+
+            if (entry.isFile) {
+                const pkg = this.readPackage(path.resolve(dirname, entry.name));
+                if (pkg) {
+                    packages.push(pkg);
+                }
+                log.debug(`adding package ${entry.name} ${packages.length}`)
+            }
+        }
+
+        return packages;
+    }
+
+    globPackages(packagesGlob: string, globOptions: ExpandGlobOptions): Array<FileSystemPackage> {
+        const packages = []
+        const packageFiles = expandGlobSync(packagesGlob, globOptions);
+        for (const file of packageFiles) {
+            console.log(`## checking file ${JSON.stringify(file)}`)
+            const pkg = this.readPackage(file.path)
+            if (pkg) {
+                console.log(`## adding package ${pkg}`)
+                packages.push(pkg)
+            }
+        }
+        return packages
+    }
+
+    private readPackage(yamlFile: string): FileSystemPackage | undefined {
+        if (!yamlFile.match(/\.levain\.ya?ml$/)) {
+            return undefined;
+        }
+
+        const fileinfo = Deno.lstatSync(yamlFile);
+        if (!fileinfo.isFile) {
+            return undefined;
+        }
+
+        const packageName = yamlFile.replace(/.*[\/|\\]/g, '').replace(/\.levain\.ya?ml/, '')
+        log.debug(`readPackage ${packageName} ${yamlFile}`);
+        const yamlStr: string = Deno.readTextFileSync(yamlFile);
+
+        const pkg = new FileSystemPackage(
+            this.config,
+            packageName,
+            this.config.replaceVars(`\${levainHome}/${packageName}`),
+            yamlFile,
+            yamlStr,
+            this);
+
+        return pkg;
+    }
+
+
 }
