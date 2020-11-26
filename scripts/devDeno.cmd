ed0f29de6b74761b7fce2020a95b56bc6ab36ebd
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-25 21:12:22 -0300

    deno - for development purposes

---
+++ scripts/devDeno.cmd
@@ -0,0 +1,23 @@
+@echo off
+
+SETLOCAL
+set myPath=%~dp0
+set levainRoot=%myPath%..
+
+if not exist %levainRoot%\bin (
+    mkdir %levainRoot%\bin
+)
+
+if not exist %levainRoot%\bin\deno.exe (
+    echo Please copy a deno.exe into %levainRoot%\bin
+    goto:eof
+)
+
+set DENO_DIR=%levainRoot%\bin
+%levainRoot%\bin\deno.exe -V info
+%levainRoot%\bin\deno.exe cache --unstable --reload %levainRoot%\src\levain.ts
+
+echo.
+echo.
+
+ENDLOCAL
