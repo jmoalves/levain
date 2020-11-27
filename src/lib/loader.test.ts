4603b785a97eac8e3e827cc4e506aee5d655f416
rafaelwalter <rafawalter@gmail.com>
2020-11-27 17:44:16 -0300

    unit test for Action or Command not found

--- src/lib/loader.test.ts
+++ src/lib/loader.test.ts
@@ -1,4 +1,4 @@
-import {assert,} from "https://deno.land/std/testing/asserts.ts";
+import {assert, assertThrows,} from "https://deno.land/std/testing/asserts.ts";
 import Loader from "./loader.ts";
 import Config from "./config.ts";
 import ListCommand from "../cmd/list_command.ts";
@@ -9,7 +9,7 @@ import AssertContainsAction from "../action/assert_contains_action.ts";
 // Commands
 //
 Deno.test('loadCommand should know the list command', () => {
-    const loader = new Loader(new Config([]))
+    const loader = getLoader()
 
     const command = loader.loadCommandStatic('list')
 
@@ -17,13 +17,44 @@ Deno.test('loadCommand should know the list command', () => {
 })
 
 
+Deno.test('should throw exception when command was not found', () => {
+    const loader = getLoader()
+
+    assertThrows(
+        () => {
+            loader.loadCommandStatic('thisCommandDoesNotExist')
+        },
+        Error,
+        'Command thisCommandDoesNotExist not found - Aborting...'
+    )
+})
+
 //
 // Actions
 //
 Deno.test('should know the assertContains action', () => {
-    const loader = new Loader(new Config([]))
+    const loader = getLoader()
 
     const action = loader.loadActionStatic('assertContains')
 
     assert(action instanceof AssertContainsAction)
 })
+
+Deno.test('should throw exception when Action does not exist', () => {
+    const loader = getLoader()
+
+    assertThrows(
+        () => {
+            loader.loadActionStatic('thisActionDoesNotExist')
+        },
+        Error,
+        'Action thisActionDoesNotExist not found - Aborting...'
+    )
+})
+
+//
+
+function getLoader() {
+    const loader = new Loader(new Config([]))
+    return loader;
+}
