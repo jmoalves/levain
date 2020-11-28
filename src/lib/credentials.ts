2f7d1743cdb78773a89a1e223c4beda16eb062de
rafaelwalter <rafawalter@gmail.com>
2020-11-28 17:24:41 -0300

    Credentials object

--- src/lib/credentials.ts
+++ src/lib/credentials.ts
@@ -3,102 +3,106 @@ import {envChain, promptSecret} from './utils.ts';
 import Config from './config.ts';
 import StringUtils from './string_utils.ts';
 
-export function askEmail(config: Config, emailDomain: string | undefined = undefined): void {
-    log.debug(`Asking for email`)
 
-    let defaultEmail = config.email;
-    if (!defaultEmail) {
-        if (config.login && emailDomain) {
-            defaultEmail = config.login + (emailDomain.startsWith("@") ? "" : "@") + emailDomain;
-            log.debug(`defaultEmail = ${defaultEmail}`);
-        } else {
-            if (!config.login) {
-                log.debug("No username for defaultEmail");
-            }
+export default class Credentials {
 
-            if (!emailDomain) {
-                log.debug("No emailDomain for defaultEmail");
+    askEmail(config: Config, emailDomain: string | undefined = undefined): void {
+        log.debug(`Asking for email`)
+
+        let defaultEmail = config.email;
+        if (!defaultEmail) {
+            if (config.login && emailDomain) {
+                defaultEmail = config.login + (emailDomain.startsWith("@") ? "" : "@") + emailDomain;
+                log.debug(`defaultEmail = ${defaultEmail}`);
+            } else {
+                if (!config.login) {
+                    log.debug("No username for defaultEmail");
+                }
+
+                if (!emailDomain) {
+                    log.debug("No emailDomain for defaultEmail");
+                }
             }
         }
-    }
 
-    let email = prompt("    Email: ", defaultEmail);
-    if (!email) {
-        throw new Error(`Unable to collect email`);
-    }
-
-    // TODO: Validate email
-
-    config.email = email;
-}
-
-export function askLogin(config: Config): void {
-    log.debug(`Asking for username`)
-
-    let login: string | null = prompt("    Login: ", envChain("user", "username")?.toLowerCase());
-    if (!login) {
-        throw new Error(`Unable to collect login`);
-    }
-
-    config.login = login;
-}
-
-export function askFullName(config: Config): void {
-    log.debug(`Asking for full name`)
-
-    console.log("What's your full name?");
-    let fullname: string | null = prompt("Full name: ", envChain("user", "fullname") || "");
-    if (!fullname) {
-        throw new Error(`Unable to collect full name`);
-    }
-
-    config.fullname = fullname;
-}
-
-export async function askPassword(config: Config) {
-    const forbiddenPasswordChars = '^&'
-
-    let tries = 0;
-    do {
-        tries++;
-        log.debug(`Asking for password, try ${tries}`)
-
-        console.log('')
-        console.log(' ========================================================================================')
-        console.log(' === ATTENTION PLEASE! The characters below are known to cause problems with passwords')
-        console.log(' === If you use one of them, please change your password and come back.')
-        console.log(' === Do not use:')
-        console.log(` === ${forbiddenPasswordChars}`)
-        console.log(' ========================================================================================')
-        console.log('')
-
-        const password = await promptSecret(" Password: ");
-        console.log("");
-
-        if (!password) {
-            console.log("Please, inform your password.");
-            console.log("");
-            continue;
+        let email = prompt("    Email: ", defaultEmail);
+        if (!email) {
+            throw new Error(`Unable to collect email`);
         }
 
-        const pw2 = await promptSecret("  Confirm: ");
-        console.log("");
+        // TODO: Validate email
 
-        if (password == pw2) {
-            if (StringUtils.textContainsAtLeastOneChar(password || '', forbiddenPasswordChars)) {
-                throw '****** INVALID CHAR IN PASSWORD. Please change your password and try again.'
+        config.email = email;
+    }
+
+    askLogin(config: Config): void {
+        log.debug(`Asking for username`)
+
+        let login: string | null = prompt("    Login: ", envChain("user", "username")?.toLowerCase());
+        if (!login) {
+            throw new Error(`Unable to collect login`);
+        }
+
+        config.login = login;
+    }
+
+    askFullName(config: Config): void {
+        log.debug(`Asking for full name`)
+
+        console.log("What's your full name?");
+        let fullname: string | null = prompt("Full name: ", envChain("user", "fullname") || "");
+        if (!fullname) {
+            throw new Error(`Unable to collect full name`);
+        }
+
+        config.fullname = fullname;
+    }
+
+    async askPassword(config: Config) {
+        const forbiddenPasswordChars = '^&'
+
+        let tries = 0;
+        do {
+            tries++;
+            log.debug(`Asking for password, try ${tries}`)
+
+            console.log('')
+            console.log(' ========================================================================================')
+            console.log(' === ATTENTION PLEASE! The characters below are known to cause problems with passwords')
+            console.log(' === If you use one of them, please change your password and come back.')
+            console.log(' === Do not use:')
+            console.log(` === ${forbiddenPasswordChars}`)
+            console.log(' ========================================================================================')
+            console.log('')
+
+            const password = await promptSecret(" Password: ");
+            console.log("");
+
+            if (!password) {
+                console.log("Please, inform your password.");
+                console.log("");
+                continue;
             }
 
+            const pw2 = await promptSecret("  Confirm: ");
             console.log("");
-            console.log("Double checked password, but we did NOT validate it with the server");
+
+            if (password == pw2) {
+                if (StringUtils.textContainsAtLeastOneChar(password || '', forbiddenPasswordChars)) {
+                    throw '****** INVALID CHAR IN PASSWORD. Please change your password and try again.'
+                }
+
+                console.log("");
+                console.log("Double checked password, but we did NOT validate it with the server");
+                console.log("");
+                config.password = password;
+                return;
+            }
+
+            console.log("Password mismatch... Please, inform again.");
             console.log("");
-            config.password = password;
-            return;
-        }
+        } while (tries < 3);
 
-        console.log("Password mismatch... Please, inform again.");
-        console.log("");
-    } while (tries < 3);
-
-    throw new Error(`Unable to collect password after ${tries} attempts`);
+        throw new Error(`Unable to collect password after ${tries} attempts`);
+    }
 }
