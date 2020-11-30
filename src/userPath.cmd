239899dddccb7651433baa008566410b1ebfc75c
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-11-30 18:16:36 -0300

    userPath - check if path is already there. Fix #29

--- src/userPath.cmd
+++ src/userPath.cmd
@@ -1,9 +1,17 @@
 @echo off
+@setlocal enableextensions enabledelayedexpansion
+
 for /f "usebackq tokens=2,*" %%A in (`reg query HKCU\Environment /v PATH`) do set levainUserPath=%%B
 
 :handleParameter
 if "a%1" == "a" goto finished
-set levainUserPath=%1;%levainUserPath%
+
+REM IF path not present, add it
+REM https://stackoverflow.com/questions/7005951/batch-file-find-if-substring-is-in-string-not-in-a-file
+set newPath=%1
+CALL SET pathRemoved=%%levainUserPath:%newPath%=%%
+if "a%pathRemoved%" == "a%levainUserPath%" set levainUserPath=%1;%levainUserPath%
+
 shift
 goto handleParameter
 
