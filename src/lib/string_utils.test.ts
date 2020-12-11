2602ecc335b33d632dd3c669a6ea78ab67d6933f
Rafael Walter <rafawalter@gmail.com>
2020-12-11 14:49:53 -0300

    parseBoolean

--- src/lib/string_utils.test.ts
+++ src/lib/string_utils.test.ts
@@ -8,80 +8,100 @@ Deno.test('should check if textContainsChars', () => {
 
 Deno.test('humanizeMillis', () => {
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().hours(50).minutes(35).seconds(28).millis(75).build()),
-                "50h 35min 28.075s");
+        "50h 35min 28.075s");
 
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().hours(2).minutes(35).seconds(28).millis(75).build()),
-                "2h 35min 28.075s");
+        "2h 35min 28.075s");
 
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().hours(2).minutes(35).seconds(28).build()),
-                "2h 35min 28s");
+        "2h 35min 28s");
 
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().hours(2).minutes(35).millis(75).build()),
-                "2h 35min 0.075s");
+        "2h 35min 0.075s");
 
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().hours(2).minutes(35).build()),
-                "2h 35min");
+        "2h 35min");
 
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().hours(2).build()),
-                "2h");
+        "2h");
 
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().minutes(35).build()),
-                "35min");
+        "35min");
 
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().seconds(28).millis(75).build()),
-                "28.075s");
+        "28.075s");
 
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().seconds(28).build()),
-                "28s");
+        "28s");
 
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().millis(75).build()),
-                "0.075s");
+        "0.075s");
 
     assertEquals(StringUtils.humanizeMillis(MillisBuilder.newMillis().build()),
-                "0.000s");
+        "0.000s");
 
     assertEquals(StringUtils.humanizeMillis(0.029),
-                "0.000s");
+        "0.000s");
 });
 
 class MillisBuilder {
-	private static readonly SECONDS = 1000;
-	private static readonly MINUTES = 60 * MillisBuilder.SECONDS;
-	private static readonly HOURS = 60 * MillisBuilder.MINUTES;
+    private static readonly SECONDS = 1000;
+    private static readonly MINUTES = 60 * MillisBuilder.SECONDS;
+    private static readonly HOURS = 60 * MillisBuilder.MINUTES;
 
-	private _hours = 0;
-	private _minutes = 0;
-	private _seconds = 0;
-	private _millis = 0;
+    private _hours = 0;
+    private _minutes = 0;
+    private _seconds = 0;
+    private _millis = 0;
 
-	public MillisBuilder() {
-	}
+    public MillisBuilder() {
+    }
 
-	public static newMillis(): MillisBuilder {
-		return new MillisBuilder();
-	}
+    public static newMillis(): MillisBuilder {
+        return new MillisBuilder();
+    }
 
-	public hours(hours: number): MillisBuilder {
-		this._hours = hours;
-		return this;
-	}
+    public hours(hours: number): MillisBuilder {
+        this._hours = hours;
+        return this;
+    }
 
-	public minutes(minutes: number): MillisBuilder {
-		this._minutes = minutes;
-		return this;
-	}
+    public minutes(minutes: number): MillisBuilder {
+        this._minutes = minutes;
+        return this;
+    }
 
-	public seconds(seconds: number): MillisBuilder {
-		this._seconds = seconds;
-		return this;
-	}
+    public seconds(seconds: number): MillisBuilder {
+        this._seconds = seconds;
+        return this;
+    }
 
-	public millis(millis: number): MillisBuilder {
-		this._millis = millis;
-		return this;
-	}
+    public millis(millis: number): MillisBuilder {
+        this._millis = millis;
+        return this;
+    }
 
-	public build():number {
-		return this._hours * MillisBuilder.HOURS + this._minutes * MillisBuilder.MINUTES + this._seconds * MillisBuilder.SECONDS + this._millis;
-	}
+    public build(): number {
+        return this._hours * MillisBuilder.HOURS + this._minutes * MillisBuilder.MINUTES + this._seconds * MillisBuilder.SECONDS + this._millis;
+    }
 }
+
+//
+// parseBoolean
+//
+Deno.test('should parse true', () => {
+    assertEquals(StringUtils.parseBoolean(true), true)
+    assertEquals(StringUtils.parseBoolean('true'), true)
+    assertEquals(StringUtils.parseBoolean('1'), true)
+    assertEquals(StringUtils.parseBoolean(1), true)
+    assertEquals(StringUtils.parseBoolean(123), true)
+    assertEquals(StringUtils.parseBoolean('any text'), true)
+})
+Deno.test('should parse false', () => {
+    assertEquals(StringUtils.parseBoolean(false), false)
+    assertEquals(StringUtils.parseBoolean('false'), false)
+    assertEquals(StringUtils.parseBoolean('0'), false)
+    assertEquals(StringUtils.parseBoolean(0), false)
+    assertEquals(StringUtils.parseBoolean(undefined), false)
+    assertEquals(StringUtils.parseBoolean(null), false)
+})
--- src/lib/string_utils.ts
+++ src/lib/string_utils.ts
@@ -7,36 +7,42 @@ export default class StringUtils {
     static humanizeMillis(millis: number): string {
         millis = Math.round(millis);
 
-		if (millis == 0) {
-			return "0.000s";
-		}
+        if (millis == 0) {
+            return "0.000s";
+        }
 
-		let seconds = Math.floor(millis / 1000);
-		millis = millis % 1000;
+        let seconds = Math.floor(millis / 1000);
+        millis = millis % 1000;
 
-		let minutes = Math.floor(seconds / 60);
-		seconds = seconds % 60;
+        let minutes = Math.floor(seconds / 60);
+        seconds = seconds % 60;
 
-		let hours = Math.floor(minutes / 60);
-		minutes = minutes % 60;
+        let hours = Math.floor(minutes / 60);
+        minutes = minutes % 60;
 
         let str = "";
         if (hours > 0) {
-			str += hours + "h" + " ";
-		}
+            str += hours + "h" + " ";
+        }
 
-		if (minutes > 0) {
-			str += minutes + "min" + " ";
-		}
+        if (minutes > 0) {
+            str += minutes + "min" + " ";
+        }
 
-		if (seconds > 0 || millis > 0) {
-			str += seconds;
-			if (millis > 0) {
-				str += "." + (""+millis).padStart(3, '0');
-			}
-			str += "s";
-		}
+        if (seconds > 0 || millis > 0) {
+            str += seconds;
+            if (millis > 0) {
+                str += "." + ("" + millis).padStart(3, '0');
+            }
+            str += "s";
+        }
 
         return str.trim();
+    }
+
+    static parseBoolean(value: any) {
+        return [null, undefined, false, 'false', 0, '0'].includes(value)
+            ? false
+            : true
     }
 }
