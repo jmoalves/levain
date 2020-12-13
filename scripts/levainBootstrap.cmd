8b962e4d7bd2cedf75fd9f42bacadb85ab1c9cdb
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-12-13 17:50:09 -0300

    levainBootstrap.cmd - Script to run levain across network drives

---
+++ scripts/levainBootstrap.cmd
@@ -0,0 +1,46 @@
+@echo off
+
+set levainVersion=%1
+if "a%levainVersion%" == "a" (
+    echo.
+    echo Inform the levain version
+    exit /b 1
+)
+shift
+
+REM FIXME: Allways expand?
+if not exist %TEMP%\levain-%levainVersion%\levain.cmd (
+    call:fnExpandLevain %levainVersion%
+)
+
+if not exist %TEMP%\levain-%levainVersion%\levain.cmd (
+    echo.
+    echo No levain %levainVersion% found 
+    exit /b 1
+)
+
+set args=
+:getArgs
+if not "a%1" == "a" set args=%args% %1& shift & goto getArgs
+
+%TEMP%\levain-%levainVersion%\levain.cmd %args%
+goto:eof
+
+
+
+:fnExpandLevain
+set levainVersion=%1
+
+set levainPath=%cd%
+
+set file=
+for %%i in (%levainPath%\levain-v%levainVersion%-*-windows-x86_64.zip) do set file=%%i
+
+if "a%file%" == "a" (
+    echo.
+    echo No levain zip found for %levainVersion%
+    exit /b 1
+)
+
+powershell.exe -nologo -noprofile -command "& { Expand-Archive -Force -LiteralPath %file% -DestinationPath %TEMP% }"
+goto:eof
