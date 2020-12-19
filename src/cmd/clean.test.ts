c013f11af2f39673c7f67f80468e2c7121581e28
rafaelwalter <rafawalter@gmail.com>
2020-12-19 11:08:21 -0300

    levain clean cache

--- src/cmd/clean.test.ts
+++ src/cmd/clean.test.ts
@@ -1,16 +1,39 @@
 import CommandFactory from "./command_factory.ts";
 import TestHelper from "../lib/test/test_helper.ts";
-import {assert} from "https://deno.land/std/testing/asserts.ts";
+import {assert, assertThrows} from "https://deno.land/std/testing/asserts.ts";
 import CleanCommand from "./clean.ts";
+import {assertDirCount} from "../lib/test/more_asserts.ts";
 
 Deno.test('CleanCommand should be in command factory', () => {
     const commandFactory = new CommandFactory()
-    let config = TestHelper.getConfig();
+    const config = TestHelper.getConfig();
 
     const command = commandFactory.get('clean', config)
 
     assert(command instanceof CleanCommand, 'should be an instance of CleanCommand')
 })
+Deno.test('CleanCommand should identify unknown option', () => {
+    const config = TestHelper.getConfig()
+    const command = new CleanCommand(config)
+
+    assertThrows(
+        () => {
+            command.execute(['--this-param-does-not-exist'])
+        },
+        Error,
+        'ERROR: Unknown option --this-param-does-not-exist',
+    )
+})
 Deno.test('CleanCommand should clean cache', () => {
-    
+    const cacheDir = TestHelper.getNewTempDir()
+    TestHelper.addRandomFilesToDir(cacheDir, 3)
+    assertDirCount(cacheDir, 3)
+
+    const config = TestHelper.getConfig()
+    config.levainCacheDir = cacheDir
+    const command = new CleanCommand(config)
+
+    command.execute([])
+
+    assertDirCount(cacheDir, 0)
 })
--- src/cmd/clean.ts
+++ src/cmd/clean.ts
@@ -1,5 +1,8 @@
 import Command from "./command.ts";
 import Config from "../lib/config.ts";
+import {parseArgs} from "../lib/parse_args.ts";
+import * as log from "https://deno.land/std/log/mod.ts";
+import {emptyDirSync} from "https://deno.land/std/fs/mod.ts";
 
 export default class CleanCommand implements Command {
 
@@ -8,8 +11,17 @@ export default class CleanCommand implements Command {
     ) {
     }
 
-    execute(args: string[]): void {
-        throw "not implemented yet"
+    execute(parameters: string[]): void {
+        log.info('CLEAN')
+
+        const myArgs = parseArgs(parameters, {
+            boolean: [
+                "cache",
+            ]
+        });
+
+        const cache = this.config.levainCacheDir;
+        emptyDirSync(cache)
     }
 
 }
