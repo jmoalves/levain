2b7d742b47439664f7bb417238b318868933e5d5
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-12-22 19:34:59 -0300

    os_utils.test - check if temp and home exist

--- src/lib/os_utils.test.ts
+++ src/lib/os_utils.test.ts
@@ -1,15 +1,21 @@
-import {assertEquals, assertNotEquals} from "https://deno.land/std/testing/asserts.ts";
+import {assert, assertEquals, assertNotEquals} from "https://deno.land/std/testing/asserts.ts";
+import { existsSync } from "https://deno.land/std@0.79.0/fs/exists.ts";
 import OsUtils from "./os_utils.ts";
 
 Deno.test('should know where is the temp folder', () => {
-    assertNotEquals(OsUtils.tempDir, undefined)
+    assertNotEquals(OsUtils.tempDir, undefined);
+    assert(existsSync(OsUtils.tempDir));
 })
+
 Deno.test('should know where is the home folder', () => {
     assertNotEquals(OsUtils.homeDir, undefined)
+    assert(existsSync(OsUtils.homeDir));
 })
+
 Deno.test('should know the users login', () => {
     assertNotEquals(OsUtils.login, undefined)
 })
+
 Deno.test('should know if we are running in Windows', () => {
     const os = Deno.build.os
     const shouldBeWindows = (os === 'windows')
