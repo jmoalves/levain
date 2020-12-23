2e1325745b0b3614088a1b43938840e03e04d536
rafaelwalter <rafawalter@gmail.com>
2020-12-23 18:20:05 -0300

    fixing tests

--- src/action/add_path.test.ts
+++ src/action/add_path.test.ts
@@ -1,6 +1,6 @@
 import ActionFactory from './action_factory.ts';
 import TestHelper from '../lib/test/test_helper.ts';
-import {assert, assertArrayIncludes, assertEquals, assertStringIncludes, assertThrowsAsync,} from "https://deno.land/std/testing/asserts.ts";
+import {assert, assertArrayIncludes,} from "https://deno.land/std/testing/asserts.ts";
 import AddPathAction from "./add_path.ts";
 
 Deno.test('should be obtainable with action factory', () => {
@@ -14,7 +14,7 @@ Deno.test('should add to path config', async () => {
     const config = TestHelper.getConfig()
     const action = new AddPathAction(config)
 
-    await action.execute(TestHelper.mockPackage(), ['C:\\test_path\\bin'])
-    
-    assertArrayIncludes(config.context.action.addpath.path, ['C:\\test_path\\bin'])
+    await action.execute(TestHelper.mockPackage(), [TestHelper.folderThatAlwaysExists])
+
+    assertArrayIncludes(config.context.action.addpath.path, [TestHelper.folderThatAlwaysExists])
 })
