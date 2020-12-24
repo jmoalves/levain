3d837e32a7cb9967bb32cfe07e63843be48505bc
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2020-12-24 12:59:58 -0300

    testAll.cmd - script to run all tests, one by one

---
+++ scripts/testAll.cmd
@@ -0,0 +1,10 @@
+@set currentFileDir=%~dp0
+@pushd %currentFileDir%..
+for /R . %%f in (*.test.ts) do (
+    cls
+    echo.
+    echo === %%f 
+    deno test --allow-all --unstable %%f
+    if errorlevel 1 pause
+)
+@popd
