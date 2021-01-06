58cd75abe924fdb3debf40882fefc78dc4bb1f7e
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-01-06 16:55:28 -0300

    Issue 77 - Fix src/lib/repository/chain_repository.test.ts

--- src/lib/repository/chain_repository.test.ts
+++ src/lib/repository/chain_repository.test.ts
@@ -5,14 +5,14 @@ import ChainRepository from "./chain_repository.ts";
 import MockRepository from "./mock_repository.ts";
 import {MockPackage} from "../package/mock_package.ts";
 
-Deno.test('chainRepo should have a name', () => {
-    const repo = getTestRepo()
+Deno.test('chainRepo should have a name', async () => {
+    const repo = await getTestRepo()
 
     assertEquals(repo.name, 'chainRepo for mockRepo1, mockRepo2')
 })
 
-Deno.test('chainRepo should list packages from repos', () => {
-    const repo = getTestRepo()
+Deno.test('chainRepo should list packages from repos', async () => {
+    const repo = await getTestRepo()
 
     const packages = repo.packages
     const packageNames = packages.map(pkg => pkg.name)
@@ -20,7 +20,7 @@ Deno.test('chainRepo should list packages from repos', () => {
     assertEquals(packageNames, ['package 1', 'package 2', 'package 3'])
 })
 
-function getTestRepo() {
+async function getTestRepo() {
     const repoMock1 = new MockRepository('mockRepo1', [
         new MockPackage('package 1', '1.0.0'),
         new MockPackage('package 2', '2.0.0'),
@@ -30,5 +30,6 @@ function getTestRepo() {
         new MockPackage('package 3', '3.0.0'),
     ])
     const repo = new ChainRepository(new Config([]), [repoMock1, repoMock2])
+    await repo.init()
     return repo
 }
