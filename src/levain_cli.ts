b6c4808ca289cb36cfa37dbd5e25cb93d0a432a3
GitHub Action <action@github.com>
2021-01-08 13:46:54 +0000

    vHEAD

--- src/levain_cli.ts
+++ src/levain_cli.ts
@@ -13,7 +13,7 @@ export default class LevainCli {
     async execute(myArgs: any = {}): Promise<void> {
         const __filename = path.fromFileUrl(import.meta.url);
 
-        log.info(`levain v0.20.2    (${__filename})`);
+        log.info(`levain vHEAD    (${__filename})`);
         log.info(`Deno   v${Deno.version.deno}`);
 
         log.debug("args " + JSON.stringify(myArgs));
