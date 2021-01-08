7834af0c104fd332041b8fb05dfaaf5484593734
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-01-08 08:02:29 -0300

    Issue 77 - fix src/lib/config.test.ts

--- src/lib/config.test.ts
+++ src/lib/config.test.ts
@@ -2,97 +2,9 @@ import {assert, assertEquals, assertMatch, assertNotEquals} from "https://deno.l
 import * as path from "https://deno.land/std/path/mod.ts";
 
 import Config from './config.ts';
-import Repository from './repository/repository.ts';
-import ChainRepository from './repository/chain_repository.ts';
-import CacheRepository from './repository/cache_repository.ts';
 import {assertStringEndsWith} from './test/more_asserts.ts';
 import TestHelper from "./test/test_helper.ts";
 
-//
-// addRepos
-//
-Deno.test('should add empty string repos', () => {
-    const config = new Config([])
-    let repos: Set<string> = new Set<string>()
-
-    config.addRepos(repos, [""])
-
-    assertEquals(0, repos.size)
-})
-
-Deno.test('should not add empty array repos', () => {
-    const config = new Config([])
-    let repos: Set<string> = new Set<string>()
-
-    config.addRepos(repos, [])
-
-    assertEquals(0, repos.size)
-})
-
-Deno.test('should not add undefined repos', () => {
-    const config = new Config([])
-    let repos: Set<string> = new Set<string>()
-
-    config.addRepos(repos, undefined)
-
-    assertEquals(0, repos.size)
-})
-//
-// addRepo
-//
-Deno.test('should not add undefined repo', () => {
-    const config = new Config([])
-    let repos: Set<string> = new Set<string>()
-
-    config.addRepo(repos, undefined)
-
-    assertEquals(0, repos.size)
-})
-
-Deno.test('should not add string "undefined" repo', () => {
-    const config = new Config([])
-    let repos: Set<string> = new Set<string>()
-
-    config.addRepo(repos, "undefined")
-
-    assertEquals(0, repos.size)
-})
-//
-// configRepo
-//
-Deno.test('should config default repos', () => {
-    class TestConfig extends Config {
-        public load(): void {
-        }
-    }
-
-    const config = new TestConfig([])
-    const myArgs = {}
-
-    const cacheRepo: Repository = config.configRepo(myArgs, false)
-
-    assert(cacheRepo instanceof CacheRepository)
-    const chainRepo = (cacheRepo as CacheRepository).repository
-    assert(chainRepo instanceof ChainRepository)
-    const repos: Repository[] = (chainRepo as ChainRepository).repositories
-    const repoUris = repos.map(it => it.absoluteURI)
-    assertEquals(repos.length, 1)
-    assertStringEndsWith(repoUris[0], "levain")
-})
-Deno.test('should add extra repo', () => {
-    const config = new Config([])
-    const myArgs = {
-        "addRepo": [
-            "D:\\git.repo\\bnd-levain-pkg\\scripts\\.."
-        ],
-    }
-
-    const cacheRepo: Repository = config.configRepo(myArgs, false)
-
-    const chainRepo = (cacheRepo as CacheRepository).repository
-    const repos: Repository[] = (chainRepo as ChainRepository).repositories
-    assertEquals(repos.length, 2)
-})
 //
 // dirs
 //
