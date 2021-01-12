13f31ca15ef6723bdc225e2892c01b8d35632751
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-01-11 23:04:24 -0300

    GitRepository - Fix #88

--- src/lib/git_utils.ts
+++ src/lib/git_utils.ts
@@ -23,7 +23,7 @@ export default class GitUtils {
         log.info(`-- GIT - PULL - ${dir}`);
         OsUtils.onlyInWindows();
 
-        const command = `cmd /u /c pushd ${dir} && ${this.gitCmd} pull --progress --no-tags --depth=1 && popd`;
+        const command = `cmd /u /c pushd ${dir} && ${this.gitCmd} pull --force -q --progress --no-tags --depth=1 --update-shallow --allow-unrelated-histories --no-commit && popd`;
         await OsUtils.runAndLog(command);
     }
 }
