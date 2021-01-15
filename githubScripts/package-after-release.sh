46f35a3cf2952a9b2fb70acf4e5b23d6296e91c8
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-01-14 23:56:27 -0300

    bootstrap - fix zip

--- githubScripts/package-after-release.sh
+++ githubScripts/package-after-release.sh
@@ -158,7 +158,7 @@ echo ${levainZipFile} created
 bootstrapZipFile=levainBootstrap-windows-x86_64.zip
 cd ${distDir}
 rm -rf extra-bin/windows/git #bootstrap does not need this
-zip -r ${distRoot}/${bootstrapZipFile} ../levainBootstrap.cmd extra-bin >/dev/null
+zip -r ${distRoot}/${bootstrapZipFile} ${distRoot}/levainBootstrap.cmd extra-bin >/dev/null
 cd - >/dev/null
 cd ${distRoot}
 sha256sum ${bootstrapZipFile} > ${bootstrapZipFile}.sha256
