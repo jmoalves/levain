e86be52928832efef88b05aface97a9780788074
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-01-18 12:45:46 -0300

    GitRepo - uses RepoFactory to create the localRepo

--- src/lib/repository/git_repository.ts
+++ src/lib/repository/git_repository.ts
@@ -1,27 +1,32 @@
 import * as log from "https://deno.land/std/log/mod.ts";
 import * as path from "https://deno.land/std/path/mod.ts";
+import { existsSync } from "https://deno.land/std/fs/exists.ts";
 
 import Package from '../package/package.ts'
 import Config from '../config.ts';
-import AbstractRepository from './abstract_repository.ts';
 import GitUtils from "../git_utils.ts";
-import { existsSync } from "https://deno.land/std/fs/exists.ts";
-import FileSystemRepository from "./file_system_repository.ts";
+
+import AbstractRepository from './abstract_repository.ts';
+import Repository from "./repository.ts";
+import RepositoryFactory from "./repository_factory.ts";
 
 export default class GitRepository extends AbstractRepository {
-    readonly name;
-    private readonly gitUtils: GitUtils;
+    readonly name
+    private readonly gitUtils: GitUtils
 
-    private readonly localDir: string;
-    private localRepo: FileSystemRepository|undefined;
+    private repoFactory: RepositoryFactory
+    private readonly localDir: string
+    private localRepo: Repository|undefined
 
     constructor(private config: Config, private rootUrl: string, private rootOnly: boolean = false) {
-        super();
+        super()
 
-        log.info(`GitRepo: Root=${this.rootUrl}`);
-        this.name = `gitRepo for ${this.rootUrl}`;
-        this.gitUtils = new GitUtils(config);
-        this.localDir = path.resolve(this.config.levainConfigDir, "gitRepos", path.basename(this.rootUrl, ".git"));
+        log.info(`GitRepo: Root=${this.rootUrl}`)
+        this.name = `gitRepo for ${this.rootUrl}`
+        this.repoFactory = new RepositoryFactory(config)
+
+        this.gitUtils = new GitUtils(config)
+        this.localDir = path.resolve(this.config.levainConfigDir, "gitRepos", path.basename(this.rootUrl, ".git"))
     }
 
     async init(): Promise<void> {
@@ -31,7 +36,7 @@ export default class GitRepository extends AbstractRepository {
             await this.gitUtils.clone(this.rootUrl, this.localDir);
         }
 
-        this.localRepo = new FileSystemRepository(this.config, this.localDir, this.rootOnly);
+        this.localRepo = this.repoFactory.create(this.localDir, this.rootOnly);
         await this.localRepo.init();
     }
 
