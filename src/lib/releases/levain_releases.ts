2b23287055597c9b38ffa57184f9153cde6c1257
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-01-20 20:54:04 -0300

    Moving HttpUtils to 'utils' dir

--- src/lib/releases/levain_releases.ts
+++ src/lib/releases/levain_releases.ts
@@ -1,7 +1,7 @@
 import * as log from "https://deno.land/std/log/mod.ts";
 
 import Config from "../config.ts";
-import HttpUtils from "../http_utils.ts";
+import HttpUtils from "../utils/http_utils.ts";
 import OsUtils from "../os_utils.ts";
 
 export default class LevainReleases { 
