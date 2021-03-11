22c5e51f75e62939b882613b1e1fbb247e23e420
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-03-11 20:20:39 -0300

    devDeno - Using deno cache

--- scripts/devDeno.cmd
+++ scripts/devDeno.cmd
@@ -24,7 +24,11 @@ if exist %levainRoot%\dist rmdir /q/s %levainRoot%\dist
 set DENO_DIR=%levainRoot%\bin
 %levainRoot%\bin\deno.exe -V info
 %levainRoot%\bin\deno.exe cache --unstable --reload %levainRoot%\src\levain.ts
+::%levainRoot%\bin\deno.exe bundle --unstable --reload %levainRoot%\src\levain.ts %levainRoot%\levain.bundle.js
 if errorlevel 1 exit /b %ERRORLEVEL%
+
+::rmdir %levainRoot%\bin\deps /q /s
+::rmdir %levainRoot%\bin\gen /q /s
 
 echo.
 echo Deno cache ok
