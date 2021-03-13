6fdd4cdb66bfadc40eed08a2ddd4b503d39c667d
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2021-03-13 15:28:16 -0300

    FSRepo - improve console feedback

---
+++ src/lib/utils/console_feedback.ts
@@ -0,0 +1,45 @@
+export default class ConsoleFeedback {
+    private static readonly text = ["-", "\\", "|", "/"]
+    private static readonly MIN_INC = 50
+
+    private idx = 0
+    private lastInc = new Date().getTime()
+
+    constructor() {}
+
+    start(msg: string|undefined = undefined) {
+        if (msg) {
+            Deno.stdout.writeSync(new TextEncoder().encode(`${msg}`))
+        }
+        this.idx = 0
+    }
+
+    show() {
+        Deno.stdout.writeSync(new TextEncoder().encode(`\r${ConsoleFeedback.text[this.idx]}`))
+        this.inc()
+
+        // busy wait
+        let ini = new Date().getTime()
+        while ((new Date().getTime() - ini) < 10);
+    }
+
+    reset(msg: string|undefined = undefined) {
+        this.idx = 0
+        if (msg) {
+            Deno.stdout.writeSync(new TextEncoder().encode(`\r${msg}\r\n`))
+        }
+    }
+
+    private inc() {
+        let now = new Date().getTime()
+        if (now < (this.lastInc + ConsoleFeedback.MIN_INC)) {
+            return
+        }
+
+        this.lastInc = now
+        this.idx++
+        if (this.idx >= ConsoleFeedback.text.length) {
+            this.idx = 0
+        }
+    }
+}
