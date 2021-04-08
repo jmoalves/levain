05eca766e53469352ea653b92ae5adf26571e3ee
Rafael <rafael.walter@bndes.gov.br>
2021-04-08 19:48:33 -0300

    AddToStartMenuAction: log.info on execute

--- src/action/add-to-start-menu.ts
+++ src/action/add-to-start-menu.ts
@@ -2,35 +2,42 @@ import Config from "../lib/config.ts";
 import Package from "../lib/package/package.ts";
 import Action from "./action.ts";
 import * as path from "https://deno.land/std/path/mod.ts";
-import { parseArgs } from "../lib/parse_args.ts";
-import { ensureDirSync } from "https://deno.land/std/fs/mod.ts";
+import {parseArgs} from "../lib/parse_args.ts";
+import {ensureDirSync} from "https://deno.land/std/fs/mod.ts";
 
 export default class AddToStartMenuAction implements Action {
 
     constructor(private config: Config) {
     }
-    async execute(pkg: Package|undefined, parameters: string[]): Promise<void> {
+
+    async execute(pkg: Package | undefined, parameters: string[]): Promise<void> {
         const args = parseArgs(parameters);
-        
-        const targetFile:string = args._[0];
+
+        const targetFile: string = args._[0];
+        const folderName = args._[1];
+        if (folderName) {
+            log.info(`ADD-TO-START-MENU ${targetFile} ${folderName}`);
+        } else {
+            log.info(`ADD-TO-START-MENU ${targetFile}`);
+        }
+
         const windowsFile = targetFile.replace("file:///", '');
         const resolvedTargetFile = path.resolve(windowsFile);
+
         //With Folder
-        if (parameters.length == 2) { 
+        if (parameters.length == 2) {
             const userProfile = Deno.env.get("USERPROFILE");
             const startMenuPath = `${userProfile}\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs`;
-            const folderName = args._[1];
             const folderPath = path.resolve(startMenuPath, folderName);
             ensureDirSync(folderPath);
-            const createShortcut = Deno.run({cmd:["extra-bin/windows/os-utils/addToStartMenuWithinFolder.cmd", resolvedTargetFile, folderName]});
+
+            const createShortcut = Deno.run({cmd: ["extra-bin/windows/os-utils/addToStartMenuWithinFolder.cmd", resolvedTargetFile, folderName]});
             const result = await createShortcut.status();
             createShortcut.close();
-        }
-    
-        else { 
-            const cmd = Deno.run({cmd:["extra-bin/windows/os-utils/addToStartMenu.cmd", resolvedTargetFile]});
+        } else {
+            const cmd = Deno.run({cmd: ["extra-bin/windows/os-utils/addToStartMenu.cmd", resolvedTargetFile]});
             const result = await cmd.status();
-            cmd.close();    
+            cmd.close();
         }
-    } 
+    }
 }
