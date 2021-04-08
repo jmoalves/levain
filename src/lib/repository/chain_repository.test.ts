14a841903d3ab055b550055e82d7c4d2d005def0
Rafael <rafael.walter@bndes.gov.br>
2021-04-08 19:28:25 -0300

    Fixing unit tests

--- src/lib/repository/chain_repository.test.ts
+++ src/lib/repository/chain_repository.test.ts
@@ -5,6 +5,8 @@ import {MockPackage} from "../package/mock_package.ts";
 
 import ChainRepository from "./chain_repository.ts";
 import MockRepository from "./mock_repository.ts";
+import VersionNumber from "../utils/version_number.ts";
+
 
 Deno.test('chainRepo should have a name', async () => {
     const repo = await getTestRepo()
@@ -23,12 +25,12 @@ Deno.test('chainRepo should list packages from repos', async () => {
 
 async function getTestRepo() {
     const repoMock1 = new MockRepository('mockRepo1', [
-        new MockPackage('package 1', '1.0.0'),
-        new MockPackage('package 2', '2.0.0'),
+        new MockPackage('package 1', new VersionNumber('1.0.0')),
+        new MockPackage('package 2', new VersionNumber('2.0.0')),
     ])
     const repoMock2 = new MockRepository('mockRepo2', [
-        new MockPackage('package 2', '2.2.2'),
-        new MockPackage('package 3', '3.0.0'),
+        new MockPackage('package 2', new VersionNumber('2.2.2')),
+        new MockPackage('package 3', new VersionNumber('3.0.0')),
     ])
     const repo = new ChainRepository(new Config([]), [repoMock1, repoMock2])
     await repo.init()
--- src/lib/repository/mock_repository.ts
+++ src/lib/repository/mock_repository.ts
@@ -2,13 +2,14 @@ import {MockPackage} from "../package/mock_package.ts";
 import Package from "../package/package.ts";
 
 import AbstractRepository from './abstract_repository.ts';
+import VersionNumber from "../utils/version_number.ts";
 
 export default class MockRepository extends AbstractRepository {
     constructor(
         public name: string = 'mockRepo',
         public packages: Array<Package> = [
-            new MockPackage('aPackage', '1.0.1'),
-            new MockPackage('anotherPackage', '0.1.2'),
+            new MockPackage('aPackage', new VersionNumber('1.0.1')),
+            new MockPackage('anotherPackage', new VersionNumber('0.1.2')),
         ],
     ) {
         super();
