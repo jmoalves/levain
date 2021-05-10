6df27982ca4dc35d0d658a43bf02f51d52418284
Rafael <rafael.walter@bndes.gov.br>
2021-05-10 17:35:57 -0300

    debounce test watch

--- scripts/testWatch.ts
+++ scripts/testWatch.ts
@@ -3,21 +3,42 @@ import OsUtils from '../src/lib/os/os_utils.ts';
 
 const watcher = Deno.watchFs('src/')
 
-runTest()
+await runTest()
+
+let lastRun: number, lastFile: string
 
 for await (const event of watcher) {
     // console.log('>>>> event', event);
     if (['create', 'modify'].indexOf(event.kind)) {
-        event.paths.forEach((file) => {
+        event.paths.forEach(async (file) => {
             if (file.endsWith('.ts') && (!file.match('\$deno\$'))) {
-                runTest(file)
+                if (!bouncing(file)) {
+                    await runTest(file)
+                }
             }
         })
     }
 }
 
+function bouncing(file: string): boolean {
+    if (!lastFile || !lastRun) {
+        lastFile = file
+        lastRun = Date.now()
+        return false
+    }
+    const timeDiff = Date.now() - lastRun
+    if (lastFile === file && timeDiff < 1000) {
+        lastRun = Date.now()
+        console.log('debouncing')
+        return true
+    }
+    lastFile = file
+    lastRun = Date.now()
+    return false
+}
+
 async function runTest(file?: string): Promise<void> {
-    OsUtils.clearConsole()
+    await OsUtils.clearConsole()
     let cmd = ['deno', 'test', '--unstable', '--allow-all'];
     const testFile =
         file?.replace(/(?:.test)?.ts$/, '.test.ts')
