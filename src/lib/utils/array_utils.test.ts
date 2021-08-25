95657cb427a60375e9e06010866d7698ddb55f8a
Rafael Walter <rafael.walter@bndes.gov.br>
2021-08-25 17:09:09 -0300

    ArrayUtils.addIfUnique

--- src/lib/utils/array_utils.test.ts
+++ src/lib/utils/array_utils.test.ts
@@ -1,7 +1,7 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { ArrayUtils } from "./array_utils.ts";
+import {assertEquals} from "https://deno.land/std/testing/asserts.ts";
+import {ArrayUtils} from "./array_utils.ts";
 
-Deno.test('ArrayUtils should remove the number from an array', () => { 
+Deno.test('ArrayUtils should remove the number from an array', () => {
     //Given an array of numbers
     const arr = [1, 2, 3];
     //When I call the remove method
@@ -9,3 +9,40 @@ Deno.test('ArrayUtils should remove the number from an array', () => {
     //Then it should have removed the item
     assertEquals(arrRemoved, [1, 3]);
 })
+//
+// makeUnique
+//
+// Deno.test('ArrayUtils.makeUnique should make int[] unique', () => {
+//     assertEquals(ArrayUtils.makeUnique([1, 2, 2, 3]), [1, 2, 3])
+// })
+//
+// addIfUnique
+//
+Deno.test('ArrayUtils.addIfUnique should add to array if new element is not there', () => {
+    assertEquals(ArrayUtils.addIfUnique([1, 2, 3], 4), [1, 2, 3, 4])
+})
+Deno.test('ArrayUtils.addIfUnique should not change array if newArray element is aready there', () => {
+    assertEquals(ArrayUtils.addIfUnique([1, 2, 3], 1), [1, 2, 3])
+})
+Deno.test('ArrayUtils.addIfUnique should add new object to array', () => {
+    const group = [{age: 1}, {age: 2}]
+    const newItem = {age: 3}
+    const expectedGroup = [{age: 1}, {age: 2}, {age: 3}]
+
+    const result = ArrayUtils.addIfUnique(group, newItem, getAge)
+
+    assertEquals(result, expectedGroup)
+})
+Deno.test('ArrayUtils.addIfUnique should reject repeated object', () => {
+    const group = [{age: 1}, {age: 2}]
+    const newItem = {age: 2}
+    const expectedGroup = [{age: 1}, {age: 2}]
+
+    const result = ArrayUtils.addIfUnique(group, newItem, getAge)
+
+    assertEquals(result, expectedGroup)
+})
+
+function getAge(obj: any): any {
+    return obj['age']
+}
--- src/lib/utils/array_utils.ts
+++ src/lib/utils/array_utils.ts
@@ -1,10 +1,33 @@
-export class ArrayUtils { 
+export class ArrayUtils {
 
-    static remove(array:any[], newItem:any) { 
-        let newArray = array.filter((item) => { 
-            return newItem != item;
-        });
-
-        return newArray;
+    static remove<T>(array: T[], itemToRemove: T) {
+        return array.filter((item) => {
+            return item != itemToRemove;
+        })
     }
+
+    static addIfUnique<T>(
+        group: T[],
+        newItem: T,
+        getKeyFn?: (element: T) => any,
+    ): T[] {
+        if (getKeyFn) {
+            const newKey = getKeyFn(newItem)
+            const found = group.some(item => getKeyFn(item) === newKey);
+            if (!found) {
+                group.push(newItem)
+            }
+        } else {
+            if (!group.includes(newItem)) {
+                group.push(newItem)
+            }
+        }
+
+        return group
+    }
+
+    static makeUnique<T>(group: T[]): T[] {
+        return group
+    }
+
 }
