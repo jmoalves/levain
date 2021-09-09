9721bf6bbaa478eaafd756e773617c159525c06b
Rafael Walter <rafael.walter@bndes.gov.br>
2021-09-09 20:48:02 -0300

    WIP - fixed ListCommand

--- src/cmd/list_command.test.ts
+++ src/cmd/list_command.test.ts
@@ -1,6 +1,5 @@
 import Config from "../lib/config.ts";
 import NullRepository from "../lib/repository/null_repository.ts";
-import TestLogger from "../lib/logger/test_logger.ts";
 import {assertArrayEndsWith} from '../lib/test/more_asserts.ts';
 
 import ListCommand from "./list_command.ts";
@@ -8,7 +7,7 @@ import TestHelper from "../lib/test/test_helper.ts";
 
 Deno.test('ListCommand should list nullRepo', async () => {
 
-    const logger = await TestLogger.setup()
+    const logger = await TestHelper.setupTestLogger()
 
     const config = new Config([]);
     config.repositoryManager.repository = new NullRepository(config)
@@ -27,18 +26,18 @@ Deno.test('ListCommand should list nullRepo', async () => {
             "INFO   no packages found",
         ]
     )
-
 })
 
 Deno.test('ListCommand should list packages available in a repo', async () => {
 
-    const logger = await TestLogger.setup()
+    const logger = await TestHelper.setupTestLogger()
+
     const config = new Config([]);
     config.repositoryManager.repository = await TestHelper.getMockRepositoryInitialized()
     const list = new ListCommand(config)
 
     list.execute()
-    
+
     assertArrayEndsWith(
         logger.messages,
         [
@@ -47,6 +46,7 @@ Deno.test('ListCommand should list packages available in a repo', async () => {
             "INFO list \"\"",
             "INFO = Repository:",
             "INFO   mockRepo",
+            "DEBUG listPackages - mockRepo",
             "INFO   2 packages found",
             "INFO ",
             "INFO == Packages",
@@ -58,7 +58,8 @@ Deno.test('ListCommand should list packages available in a repo', async () => {
 
 Deno.test('ListCommand should list packages that match a search string', async () => {
     const searchString = 'another'
-    const logger = await TestLogger.setup()
+
+    const logger = await TestHelper.setupTestLogger()
     const config = new Config([]);
     config.repositoryManager.repository = await TestHelper.getMockRepositoryInitialized()
     const list = new ListCommand(config)
@@ -73,12 +74,11 @@ Deno.test('ListCommand should list packages that match a search string', async (
             "INFO list \"another\"",
             "INFO = Repository:",
             "INFO   mockRepo",
+            "DEBUG listPackages - mockRepo",
             "INFO   1 of 2 packages found",
             "INFO ",
             "INFO == Package",
             "INFO    anotherPackage                 0.1.2      => /mock/anotherPackage-0.1.2.yml",
-            "INFO    aPackage                       1.0.1      => /mock/aPackage-1.0.1.yml",
         ]
     )
 })
-
