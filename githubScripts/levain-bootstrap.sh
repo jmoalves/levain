cd7281f704accbdc12addf7d2ef50282ab901b72
João Mauricio de O. Alves <jmauricio@pobox.com>
2021-12-05 10:34:35 -0300

    GitHub release - create levainBootstrap

---
+++ githubScripts/levain-bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd && cd - >/dev/null 2>&1 )"
+
+# FIXME: Check parameters
+levainDir=$1
+bootstrapZipFile=$2
+
+workDir=$( mktemp -d )
+
+mkdir -p ${workDir}/extra-bin/windows
+for utility in 7-zip curl; do
+    cp -r ${levainDir}/extra-bin/windows/${utility} ${workDir}/extra-bin/windows
+done
+cp ${levainDir}/levainBootstrap.cmd ${workDir}
+
+cd ${workDir}
+zip -r ${bootstrapZipFile} levainBootstrap.cmd extra-bin >/dev/null
+cd - >/dev/null
+rm -rf ${workDir}
+
+cd $( dirname $bootstrapZipFile )
+sha256sum $( basename ${bootstrapZipFile} ) > ${bootstrapZipFile}.sha256
+cd - >/dev/null
