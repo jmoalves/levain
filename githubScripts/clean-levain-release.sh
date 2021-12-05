da1130c456b7dcf8207c93a4fd908f562dd6a30a
João Mauricio de O. Alves <jmauricio@pobox.com>
2021-12-05 11:29:24 -0300

    clean-levain-release.sh fix

--- githubScripts/clean-levain-release.sh
+++ githubScripts/clean-levain-release.sh
@@ -2,30 +2,15 @@
 
 scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd && cd - >/dev/null 2>&1 )"
 
-# FIXME: Check parameters
 levainDir=$1
-
-echo CLEAN $levainDir
-for dir in \
-        ${levainDir}/bootstrap \
-        ${levainDir}/scripts \
-        ${levainDir}/githubScripts \
-        ${levainDir}/ci \
-        #${levainDir}/src \
-        #${levainDir}/levain.ts \
-        ${levainDir}/testdata \
-        #${levainDir}/bin/deps \
-        #${levainDir}/bin/gen \
-        ; do
-    if ! rm -rvf ${dir}; then
-        echo ERROR removing $dir
-        exit 1
-    fi
-done
-
-if ! find ${levainDir} -name '*.test.ts' -exec rm -vf {} \; ; then
-    echo ERROR Cleaning levain
+if [ ! -d "$levainDir"]; then
+    echo $levainDir NOT FOUND
     exit 1
 fi
 
-echo CLEAN $levainDir - DONE
+rm -rvf ${levainDir}/bootstrap &&
+rm -rvf ${levainDir}/scripts &&
+rm -rvf ${levainDir}/githubScripts &&
+rm -rvf ${levainDir}/ci &&
+rm -rvf ${levainDir}/testdata &&
+find ${levainDir} -name '*.test.ts' -exec rm -vf {} \;
