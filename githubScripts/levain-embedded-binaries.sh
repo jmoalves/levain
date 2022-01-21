3ba9a86edce21c8e7d8829890e15d916aaed1c63
João Mauricio de O. Alves <jmauricio@pobox.com>
2022-01-20 21:31:41 -0300

    BUILD - Embedded binaries - removing curl + 7-Zip + git

--- githubScripts/levain-embedded-binaries.sh
+++ githubScripts/levain-embedded-binaries.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-downloadBinary() {
-  strip=false;
+debug=false
+echoErr() { printf "ERR: %s\n" "$*" >&2; }
+echoDebug() { $debug && printf "DEBUG: %s\n" "$*" >&2; }
 
+downloadBinary() {
+  # https://stackoverflow.com/questions/16654607/using-getopts-inside-a-bash-function
+  local OPTIND OPTARG o item url strip binDir
+  strip=false;
   while getopts "i:u:sd:" o; do
   case "${o}" in
   i)
@@ -38,15 +43,14 @@ downloadBinary() {
   rm -rf ${binDir}
 
   echo = UNZIP to ${binDir}
-  unzip ${tempFile} -d ${binDir}
+  ${zipTool} x -o${binDir} -aoa ${tempFile} > /dev/null
 
   if $strip; then
     echo = STRIP ${binDir}
     for dir in ${binDir}/*; do
-      echo strip $dir
-      cd $dir
-      mv -v * ${binDir}
-      cd -
+      cd $dir > /dev/null
+      mv * ${binDir}
+      cd - > /dev/null
       rmdir $dir
     done
   fi
@@ -72,15 +76,48 @@ if [ -z "$denoWindowsDir" ]; then
   exit 1
 fi
 
-# Deno embedded
-echo Deno embedded
+# # Deno embedded
+echo = Deno embedded
 rm -rf ${levainDir}/bin
 mkdir -p ${levainDir}/bin
 cp $denoWindowsDir/deno.exe ${levainDir}/bin
+
+# 7Zip - Linux
+echo = 7Zip - Tool
+zipDir=$( mktemp -d )
+rm -rf $zipDir
+mkdir -p $zipDir
+curl -ks -o ${zipDir}/7z.tar.xz https://www.7-zip.org/a/7z2107-linux-x64.tar.xz
+tar xf ${zipDir}/7z.tar.xz --xz -C ${zipDir}
+zipTool=${zipDir}/7zzs
+
 
 # EXTRA-BIN: curl
 curlVersion=7.81.0
 curlUrl=https://curl.se/windows/dl-${curlVersion}/curl-${curlVersion}-win64-mingw.zip
 curlDir=${levainDir}/extra-bin/windows/curl
 
+echo
 downloadBinary -i curl -u $curlUrl -d $curlDir -s
+
+
+# EXTRA-BIN: git
+gitVersion=2.34.1
+gitUrl=https://github.com/git-for-windows/git/releases/download/v${gitVersion}.windows.1/MinGit-${gitVersion}-64-bit.zip
+gitDir=${levainDir}/extra-bin/windows/git
+
+echo
+downloadBinary -i git -u $gitUrl -d $gitDir
+
+
+# EXTRA-BIN: 7-Zip
+sevenUrl=https://www.7-zip.org/a/7z2107-x64.exe
+sevenDir=${levainDir}/extra-bin/windows/7-zip
+
+echo
+downloadBinary -i "7-Zip" -u $sevenUrl -d $sevenDir
+
+## CLEANUP
+echo
+echo = Cleanup
+rm -rf $zipDir
