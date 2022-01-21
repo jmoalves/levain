ee7a37710f3356c05d88f75dd7c8ca183fc99acb
João Mauricio de O. Alves <jmauricio@pobox.com>
2022-01-20 23:24:23 -0300

    GitHubActions - package binaries

---
+++ githubScripts/levain-package-binaries.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# FIXME: Check parameters
+levainDir=$1
+binZipFile=$2
+
+## Create Levain zip
+cd ${levainDir}
+zip -9r ${binZipFile} extra-bin >/dev/null
+cd - >/dev/null
+
+cd $( dirname ${binZipFile} )
+sha256sum $( basename ${binZipFile} ) > ${binZipFile}.sha256
+cd - >/dev/null
+
+echo
+echo ${binZipFile} created
+
+echo
+unzip -l ${binZipFile}
+
+echo
+echo ${binZipFile}.sha256
+cat ${binZipFile}.sha256
