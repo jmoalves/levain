281036d1903e689dbd38180ba4f5e4699799f9c7
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2022-05-29 18:44:39 -0300

    i18n - input_email

--- src/lib/user_info/input_email.ts
+++ src/lib/user_info/input_email.ts
@@ -1,11 +1,13 @@
 import {EmailValidator} from "./validators/validators.ts";
 import {Input} from 'https://deno.land/x/cliffy/prompt/input.ts'
 
+import t from '../i18n.ts'
+
 export class InputEmail {
 
     static async inputAndValidate(defaultValue: string): Promise<string> {
         return await Input.prompt({
-                message: "Do you have an EMAIL? (press return to confirm default value)",
+                message: t("lib.user_info.input_email.emailPrompt"),
                 default: defaultValue,
                 validate: EmailValidator.validate,
             }
