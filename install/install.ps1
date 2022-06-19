02f67a674c56e277bdaf22479b0b72385807cb70
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2022-06-19 11:39:19 -0300

    install - first attempt

---
+++ install/install.ps1
@@ -0,0 +1,45 @@
+#!/usr/bin/env pwsh
+# Inspired by https://deno.land/x/install@v0.1.4/install.ps1
+# TODO(everyone): Keep this script simple and easily auditable.
+
+$ErrorActionPreference = 'Stop'
+
+if ($args.Length -eq 1) {
+  $Version = $args.Get(0)
+}
+
+$LevainHome = if ($env:LEVAIN_HOME) {
+  "$env:LEVAIN_HOME"
+} else {
+  "$Home\levain"
+}
+
+# GitHub requires TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+$LevainUri = if (!$Version) {
+  "https://github.com/jmoalves/levain/releases/latest/download/levain-windows-x86_64.zip"
+} else {
+  "https://github.com/jmoalves/levain/releases/download/v${Version}/levain-v${Version}-windows-x86_64.zip"
+}
+
+$TempLevain = "$TEMP\levain"
+$TempLevainZip = "$TempLevain\levain-v${Version}-windows-x86_64.zip"
+$TempLevainDir = "$TempLevain\levain-v${Version}"
+if (!(Test-Path $TempLevain)) {
+  New-Item $TempLevain -ItemType Directory | Out-Null
+}
+
+if (!(Test-Path $TempLevainZip)) {
+  Invoke-WebRequest $LevainUri -OutFile $TempLevainZip -UseBasicParsing
+}
+
+if (!(Test-Path $TempLevainDir)) {
+  if (Get-Command Expand-Archive -ErrorAction SilentlyContinue) {
+    Expand-Archive $TempLevainZip -Destination $TempLevainDir -Force
+  } else {
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
+    [IO.Compression.ZipFile]::ExtractToDirectory($TempLevainZip, $TempLevainDir)
+}
+
+Write-Output "Levain at $TempLevainDir"
