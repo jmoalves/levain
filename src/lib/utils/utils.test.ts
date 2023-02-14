1a77a5075f2e990b734ad45638d1433a98aa2faa
Joao Mauricio de O. Alves <jmaur@bndes.gov.br>
2023-02-14 18:57:27 -0300

    Retry - increase amount and sleepTime

--- src/lib/utils/utils.test.ts
+++ src/lib/utils/utils.test.ts
@@ -3,10 +3,18 @@ import {
     assertNotEquals,
 } from "https://deno.land/std/testing/asserts.ts";
 
-import {homedir} from "./utils.ts";
+import { homedir, retry } from "./utils.ts";
 
 Deno.test("Utils", () => {
     const home = homedir()
 
     assertNotEquals(home, undefined);
+})
+
+Deno.test("retry", async () => {
+    const startTime = performance.now()
+    await retry(3, () => { throw "Abort" })
+    const endTime = performance.now()
+
+    console.log(`Test took ${endTime - startTime}ms`)
 })
