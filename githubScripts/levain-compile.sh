38b07965ca43d3f78db66e21374f8fe91db88db4
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2023-11-12 18:18:03 +0000

    Compile - first attempt

--- githubScripts/levain-compile.sh
+++ githubScripts/levain-compile.sh
@@ -3,8 +3,9 @@
 scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd && cd - >/dev/null 2>&1 )"
 
 # FIXME: Check parameters
-levainDir=$1
-myDeno=$2
+myDeno=$1
+levainSrcDir=$2
+levainBinDir=$3
 
 levainCompile() {
     # https://stackoverflow.com/questions/16654607/using-getopts-inside-a-bash-function
@@ -30,20 +31,16 @@ levainCompile() {
         --reload \
         --allow-read --allow-write --allow-env --allow-net --allow-run \
         --target ${target} --output ${output} \
-        ${levainDir}/levain.ts
+        ${levainSrcDir}/levain.ts
 }
 
 # Create levain executable
-export DENO_DIR=${levainDir}/bin
-mkdir -p ${DENO_DIR}
-${myDeno} info
-
 ## Target options
 # x86_64-unknown-linux-gnu
 # x86_64-pc-windows-msvc,
 # x86_64-apple-darwin
 # aarch64-apple-darwin
 ##
-levainCompile -t x86_64-unknown-linux-gnu -o levain
+levainCompile -t x86_64-unknown-linux-gnu -o ${levainBinDir}/levain
 #levainCompile -t x86_64-apple-darwin -o levain-mcos
-levainCompile -t x86_64-pc-windows-msvc -o levain.exe
+levainCompile -t x86_64-pc-windows-msvc -o ${levainBinDir}/levain.exe
