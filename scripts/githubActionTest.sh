518d8fa6ced077e77c94dedd3101082465119a5c
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2023-11-12 22:17:03 +0000

    fix runner.temp

--- scripts/githubActionTest.sh
+++ scripts/githubActionTest.sh
@@ -1,17 +1,2 @@
 #!/bin/bash
-
-## TEMP dirs
-actWorkDir=$( mktemp /tmp/levain-act.XXXXXXXXXXXXXXXXX -d )
-RUNNER_TEMP=${actWorkDir}/runner
-GITHUB_WORKSPACE=${actWorkDir}/ghWorkspace
-
-mkdir -p ${RUNNER_TEMP}
-mkdir -p ${GITHUB_WORKSPACE}
-
-## ACT running GitHub actions
-act \
-    --env RUNNER_TEMP=${RUNNER_TEMP} --var runner.temp=${RUNNER_TEMP} \
-    --env GITHUB_WORKSPACE=${GITHUB_WORKSPACE} --var github.workspace=${GITHUB_WORKSPACE} \
-    --rm \
-    $*
-rm -rf ${actWorkDir}
+act --rm $*
