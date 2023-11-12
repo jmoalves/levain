e307f0ff3ce48b027b3e6595d2a97cd971598c09
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2023-11-12 17:52:02 +0000

    levain-compile - linux + windows

--- githubScripts/levain-compile.sh
+++ githubScripts/levain-compile.sh
@@ -6,11 +6,44 @@ scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd && cd -
 levainDir=$1
 myDeno=$2
 
+levainCompile() {
+    # https://stackoverflow.com/questions/16654607/using-getopts-inside-a-bash-function
+    local OPTIND OPTARG o target output
+    target=
+    output=
+
+    while getopts "t:o:" o; do
+    case "${o}" in
+    t)
+        target="${OPTARG}"
+        ;;
+
+    o)
+        output="${OPTARG}"
+        ;;
+    esac
+    done
+    shift $((OPTIND - 1))
+
+    ${myDeno} compile \
+        --unstable \
+        --reload \
+        --allow-read --allow-write --allow-env --allow-net --allow-run \
+        --target ${target} --output ${output} \
+        ${levainDir}/levain.ts
+}
+
 # Create levain executable
 export DENO_DIR=${levainDir}/bin
 mkdir -p ${DENO_DIR}
 ${myDeno} info
-${myDeno} compile \
-    --unstable \
-    --allow-read --allow-write --allow-env --allow-net --allow-run \
-    ${levainDir}/levain.ts
+
+## Target options
+# x86_64-unknown-linux-gnu
+# x86_64-pc-windows-msvc,
+# x86_64-apple-darwin
+# aarch64-apple-darwin
+##
+levainCompile -t x86_64-unknown-linux-gnu -o levain
+#levainCompile -t x86_64-apple-darwin -o levain-mcos
+levainCompile -t x86_64-pc-windows-msvc -o levain.exe
