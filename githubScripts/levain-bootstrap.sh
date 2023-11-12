e75ea398e70e4c447cdac362068444ae2f6e1548
João Mauricio de O. Alves <jmoalves.dev@pobox.com>
2023-11-12 22:17:03 +0000

    fix runner.temp

--- githubScripts/levain-bootstrap.sh
+++ githubScripts/levain-bootstrap.sh
@@ -3,16 +3,17 @@
 scriptPath="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd && cd - >/dev/null 2>&1 )"
 
 # FIXME: Check parameters
-levainDir=$1
-bootstrapZipFile=$2
+levainSrcDir=$1
+levainBinDir=$2
+bootstrapZipFile=$3
 
 workDir=$( mktemp -d )
 
 mkdir -p ${workDir}/extra-bin/windows
 for utility in 7-zip curl; do
-    cp -r ${levainDir}/extra-bin/windows/${utility} ${workDir}/extra-bin/windows
+    cp -r ${levainBinDir}/extra-bin/windows/${utility} ${workDir}/extra-bin/windows
 done
-cp ${levainDir}/bootstrap/levainBootstrap.cmd ${workDir}
+cp ${levainSrcDir}/bootstrap/levainBootstrap.cmd ${workDir}
 
 cd ${workDir}
 zip -9r ${bootstrapZipFile} levainBootstrap.cmd extra-bin >/dev/null
