537cb83f947d63560f0531d9364bcd01096b6875
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-02-12 17:46:15 -0300

    test - deno path

--- scripts/test.cmd
+++ scripts/test.cmd
@@ -1,25 +1,49 @@
 @echo off
-set scriptDir=%~dp0
-set levainRoot=%scriptDir%..
+
+SETLOCAL EnableDelayedExpansion
+
+set myPath=%~dp0
+set myPath=%mypath:~0,-1%
+set levainRoot=%myPath%\..
+
+set myDenoDir=%1
+if "a%myDenoDir%" == "a" (
+    set myDenoDir=%levainRoot%\bin
+    echo WARN - Using deno default at !myDenoDir!
+)
+
+if not exist %myDenoDir% (
+    echo Can\'t find dir %myDenoDir%
+    exit 1
+)
+
+set myDenoExe=%myDenoDir%\deno.exe
+if not exist %myDenoExe% (
+    echo We can\'t find %myDenoExe%
+    exit 1
+)
+
+set DENO_DIR=%myDenoDir%
+echo Deno at %myDenoExe%
+%myDenoExe% -V info
 
 pushd %levainRoot%
 
-%levainRoot%\bin\deno -V
-
 :: Check syntax
 echo === Syntax check - sources
-%levainRoot%\bin\deno check %levainRoot%\levain.ts
+%myDenoExe% check levain.ts
 if errorlevel 1 goto:eof
 
 echo === Syntax check - tests
-%levainRoot%\bin\deno test --no-run
+%myDenoExe% test --no-run
 if errorlevel 1 goto:eof
 
 :: Test!
 echo.
 echo Running tests
 @echo on
-%levainRoot%\bin\deno test --allow-all %*
+%myDenoExe% test --allow-all
 @echo off
 
 popd
+ENDLOCAL
