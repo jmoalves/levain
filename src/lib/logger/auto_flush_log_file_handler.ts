b2f1ee8c336e9f33b48881d5c7b3c6032a35b6f6
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-02-12 13:52:44 -0300

    Deno.std - log.FileHandler move

--- src/lib/logger/auto_flush_log_file_handler.ts
+++ src/lib/logger/auto_flush_log_file_handler.ts
@@ -1,6 +1,6 @@
 import * as log from "https://deno.land/std/log/mod.ts";
 
-export class AutoFlushLogFileHandler extends log.handlers.FileHandler {
+export class AutoFlushLogFileHandler extends log.FileHandler {
     constructor(
         // levelName: LevelName,
         // options: HandlerOptions,
