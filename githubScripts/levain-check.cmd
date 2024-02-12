dddc71e95dd11db59f1c8b15ea424184395459b2
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-02-12 15:06:48 -0300

    checkSources - fix deno.exe check

--- githubScripts/levain-check.cmd
+++ githubScripts/levain-check.cmd
@@ -12,18 +12,24 @@ if "a%myDeno%" == "a" (
 )
 
 if not exist %myDeno% (
-    echo We can\'t run %myDeno%
+    echo We can\'t find %myDeno%
     exit 1
 )
 
-echo Deno at %myDeno%
+set myDenoExe=%myDeno%\deno.exe
+if not exist %myDenoExe% (
+    echo We can\'t find %myDenoExe%
+    exit 1
+)
+
+echo Deno at %myDenoExe%
 
 set tempPath=%TEMP%\denoTemp
 if exist %tempPath% rmdir /q /s %tempPath%
 mkdir %tempPath%
 
 set DENO_DIR=%tempPath%
-%myDeno% check --reload levain.ts
+%myDenoExe% check --reload levain.ts
 if errorlevel 1 (
     echo ERROR compiling levain
     rmdir /q/s %tempPath%
