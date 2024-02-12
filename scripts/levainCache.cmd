37b7daba288453e4f23dfb7ab7d2a3600a5d5c6b
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-02-12 13:52:24 -0300

    levainCache.cmd - deno check after deno cache

--- scripts/levainCache.cmd
+++ scripts/levainCache.cmd
@@ -14,7 +14,7 @@ echo === Deno cache
 set DENO_DIR=%levainRoot%\bin
 %levainRoot%\bin\deno.exe -V info
 %levainRoot%\bin\deno.exe cache --reload %levainRoot%\levain.ts
-%levainRoot%\bin\deno.exe check --reload %levainRoot%\levain.ts
+%levainRoot%\bin\deno.exe check %levainRoot%\levain.ts
 if errorlevel 1 exit /b %ERRORLEVEL%
 
 ENDLOCAL
