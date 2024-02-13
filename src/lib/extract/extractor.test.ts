fd9a440dd01f228ea9fcdc36561ce848170f2e1f
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-02-13 00:09:36 -0300

    extractor.test - workaround Deno issue 15425

--- src/lib/extract/extractor.test.ts
+++ src/lib/extract/extractor.test.ts
@@ -4,6 +4,7 @@ import TestHelper from "../test/test_helper.ts";
 import {assertFolderIncludes} from "../test/more_asserts.ts";
 import {DenoZip} from "./denozip_extractor.ts";
 
+// Workaround - https://github.com/denoland/deno/issues/15425
 Deno.test('Extractor should copy srcFile to dstFile', async () => {
     // Given
     const extractor = getExtractor();
@@ -15,6 +16,9 @@ Deno.test('Extractor should copy srcFile to dstFile', async () => {
     await extractor.copy(srcFile, dstFile)
     // Then
     assertFolderIncludes(dst, [dstFile])
+
+    // Workaround - https://github.com/denoland/deno/issues/15425
+    await new Promise((resolve) => setTimeout(resolve, 0));
 })
 
 function getExtractor() {
