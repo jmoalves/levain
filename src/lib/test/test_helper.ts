32920f02941aebb771ba319304f4a395ca047794
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-02-12 22:54:50 -0300

    TestHelper - test path pattern

--- src/lib/test/test_helper.ts
+++ src/lib/test/test_helper.ts
@@ -100,7 +100,7 @@ export default class TestHelper {
     }
 
     static getNewTempDir(): string {
-        const tempDir = Deno.makeTempDirSync({prefix: 'levain-', suffix: ".tmp"});
+        const tempDir = Deno.makeTempDirSync({prefix: 'levain-test-', suffix: ".dir.tmp"});
         console.debug(`getNewTempDir ${tempDir}`)
 
         TestHelper.removeOnExit(tempDir)
@@ -109,7 +109,7 @@ export default class TestHelper {
     }
 
     static getNewTempFile(copyFile?: string): string {
-        const fileName = Deno.makeTempFileSync()
+        const fileName = Deno.makeTempFileSync({prefix: 'levain-test-', suffix: ".file.tmp"})
         if (copyFile) {
             copySync(copyFile, fileName, {overwrite: true})
         }
