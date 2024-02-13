995095202fbedc1bc74c80d526bd412857990183
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-02-12 21:38:09 -0300

    test.cmd - option for denoDir

--- scripts/test.cmd
+++ scripts/test.cmd
@@ -6,7 +6,28 @@ set myPath=%~dp0
 set myPath=%mypath:~0,-1%
 set levainRoot=%myPath%\..
 
-set myDenoDir=%1
+:getOpts
+if "a%1" == "a" goto endGetOpts
+if "%1" == "--" goto endGetOpts
+
+if /I "%1" == "--denoDir" (
+    if "a%2" == "a" (
+        echo You must inform the denoDir
+        exit 1
+    )
+
+    set myDenoDir=%2& shift & shift & goto getOpts
+)
+
+:: Invalid option?
+set checkOption=%1
+if /i "%checkOption:~0,1%"=="-" (
+    echo Unknown option %1
+    exit /b 1
+)
+
+:endGetOpts
+
 if "a%myDenoDir%" == "a" (
     set myDenoDir=%levainRoot%\bin
     echo WARN - Using deno default at !myDenoDir!
@@ -38,12 +59,34 @@ echo === Syntax check - tests
 %myDenoExe% test --no-run
 if errorlevel 1 goto:eof
 
-:: Test!
 echo.
+
+:: Test!
+@REM echo Removing previous coverage reports
+@REM if exist coverage\ rmdir /q/s coverage\
+
+set coverOps=--coverage
+:: If we got a specific test to run, skip coverage
+if not "a%1" == "a" set coverOps=
+
 echo Running tests
 @echo on
-%myDenoExe% test --allow-all --coverage
+%myDenoExe% test --allow-all %coverOps% %*
 @echo off
+
+if "a%coverOps%" == "a" goto skipCoverage
+
+echo.
+echo Coverage report - TEXT
+%myDenoExe% coverage
+
+echo Coverage report - LCOV
+%myDenoExe% coverage --lcov --output=coverage/levain.lcov
+
+echo Coverage report - HTML
+%myDenoExe% coverage --html
+
+:skipCoverage
 
 popd
 ENDLOCAL
