be1bed88cb2a1c8390e0969207ef8a53077edd0b
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-02-15 08:51:48 -0300

    levain-build - dir fix

--- githubScripts/levain-build.sh
+++ githubScripts/levain-build.sh
@@ -57,5 +57,5 @@ mkdir -p ${targetDir}/build
 levainCompile -t x86_64-pc-windows-msvc -o ${targetDir}/build/levain.exe
 cp -rv ${levainSrcDir}/levain.cmd ${targetDir}/
 cp -rv ${levainSrcDir}/LICENSE ${targetDir}/
-cp -rv ${levainSrcDir}/recipes/ ${targetDir}/recipes/
-cp -rv ${levainSrcDir}/extra-bin/ ${targetDir}/extra-bin/
+cp -rv ${levainSrcDir}/recipes/ ${targetDir}/build/recipes/
+cp -rv ${levainSrcDir}/extra-bin/ ${targetDir}/build/extra-bin/
