687b453f5ef4b69c2d4348edb8340b2327c6dfee
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-03-19 22:22:41 -0300

    install with prompt

--- src/cmd/install.ts
+++ src/cmd/install.ts
@@ -257,13 +257,12 @@ export default class Install implements Command {
             log.debug(`- SAVE-COPY  ${src} => ${dst}`);
             copySync(src, dst);
 
-            const fileInfo = Deno.statSync(src);
-            log.info(`src info=${fileInfo}`)
-
             if (pkg.yamlItem("levain.preserveBaseDirOnUpdate")) {
                 log.info(t("cmd.install.keepingBaseDir", { pkg: pkg.name }));
                 return true;
             }
+
+            prompt(`Hit ENTER to continue saving ${src} => ${dst}`)
 
             let deletedDir = Deno.makeTempDirSync({
                 dir: path.dirname(src),
