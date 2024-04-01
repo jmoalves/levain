244553dae4c5b0a6e588005d03bb9a056135c0d3
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-04-01 20:03:36 -0300

    devLevain.cmd - handle deno version

--- scripts/devLevain.cmd
+++ scripts/devLevain.cmd
@@ -5,6 +5,8 @@ set myPath=%~dp0
 set myPath=%mypath:~0,-1%
 set levainRoot=%myPath%\..
 
+set denoVersion=%1
+
 set PWS=powershell.exe -ExecutionPolicy Bypass -NoLogo -NonInteractive -NoProfile
 
 set proxyCfg=
@@ -15,13 +17,23 @@ if not exist %levainRoot%\bin (
     mkdir %levainRoot%\bin
 )
 
-echo.
-echo === Deno latest
+set denoUrl=
+if "a%denoVersion%" == "a" (
+    echo === Deno latest
+    set denoUrl=https://github.com/denoland/deno/releases/latest/download/deno-x86_64-pc-windows-msvc.zip
+) else (
+    echo === Deno version %denoVersion%
+    set denoUrl=https://github.com/denoland/deno/releases/download/v%denoVersion%/deno-x86_64-pc-windows-msvc.zip
+)
+
+echo --- RM Deno temp dir
 set denoPath=%TEMP%\deno
 if exist %denoPath% rmdir /q /s %denoPath%
 mkdir %denoPath%
 
-%PWS% Invoke-WebRequest %proxyCfg% https://github.com/denoland/deno/releases/latest/download/deno-x86_64-pc-windows-msvc.zip -OutFile %denoPath%\deno-x86_64-pc-windows-msvc.zip
+
+echo --- Deno from %denoUrl%
+%PWS% Invoke-WebRequest %proxyCfg% %denoUrl% -OutFile %denoPath%\deno-x86_64-pc-windows-msvc.zip
 %PWS% Expand-Archive %denoPath%\deno-x86_64-pc-windows-msvc.zip -DestinationPath %levainRoot%\bin -Force
 rmdir /q /s %denoPath%
 
@@ -38,8 +50,11 @@ if exist %levainRoot%\extra-bin\windows\git rmdir /q /s %levainRoot%\extra-bin\w
 %PWS% Expand-Archive %binPath%\levain-extra-bin-windows-x86_64.zip -DestinationPath %levainRoot%
 rmdir /q /s %binPath%
 
+echo.
+echo ==================
 call %myPath%\levainCache.cmd
 if errorlevel 1 exit /b %ERRORLEVEL%
+echo ==================
 
 echo.
 echo === Dev Levain - OK
