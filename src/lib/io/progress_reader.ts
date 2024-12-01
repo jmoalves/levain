2f12ca628b5c1bc216f4d6ce3541d9ec0001c9a9
Joao Mauricio de O. Alves <jmauricio@pobox.com>
2024-12-01 10:11:11 -0300

    Deno.Closer/Writer/Reader with std/io/types

--- src/lib/io/progress_reader.ts
+++ src/lib/io/progress_reader.ts
@@ -1,7 +1,9 @@
+import type { Reader, Closer } from "jsr:@std/io/types";
+
 import RewindReader from "../io/rewind_reader.ts";
 import Timestamps from "../io/timestamps.ts";
 import Progress from "../io/progress.ts";
 
-export default interface ProgressReader extends Deno.Reader, Progress, RewindReader, Timestamps, Deno.Closer {
+export default interface ProgressReader extends Reader, Progress, RewindReader, Timestamps, Closer {
     readonly name: string
 }
