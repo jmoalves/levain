eb053c7e4984ab6fe0e1adc7b4e30d7449ee43e8
rafawalter <12330+rafawalter@users.noreply.github.com>
2025-06-27 12:00:24 -0300

    test.cmd: exit /b %ERRORLEVEL_TESTES%

--- scripts/test.cmd
+++ scripts/test.cmd
@@ -97,7 +97,7 @@ echo Running tests
 @echo on
 %myDenoExe% test --allow-all --parallel %coverOps% %allParameters%
 @echo off
-set ERRORLEVEL_TESTES='%ERRORLEVEL%'
+set ERRORLEVEL_TESTES=%ERRORLEVEL%
 if not "%ERRORLEVEL_TESTES%"=="0" (
     exit /b %ERRORLEVEL_TESTES%
 )
