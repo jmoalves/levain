b31dfc997c6be78f06781e3657cc518ab50c136e
Joao Felipe Nicolaci Pimentel <joao.pimentel@bndes.gov.br>
2026-07-23 16:17:51 -0300

    redistribute responsibilities of activate pre and cmd to fix activation

--- src/action/activate/activate_cmd.ts
+++ src/action/activate/activate_cmd.ts
@@ -3,6 +3,7 @@ import Config from "../../lib/config.ts";
 import Action from "../action.ts";
 import { parseArgs } from "../../lib/parse_args.ts";
 import { OsShell } from "../../lib/os/os_shell.ts";
+import Shell from "../../cmd/shell.ts";
 
 export default class ActivateCmd implements Action {
   constructor(private config: Config) {
@@ -14,28 +15,28 @@ export default class ActivateCmd implements Action {
         "shell",
       ],
     });
-    let pkgNames = myArgs?._;
-    let curDirPkg = undefined;
-
-    if (pkgNames.length == 0) {
-      curDirPkg = await this.config.repositoryManager.currentDirPackage();
-      if (curDirPkg && curDirPkg.dependencies && curDirPkg.dependencies.length > 0) {
-        pkgNames = curDirPkg.dependencies;
-      } else {
-        pkgNames = [this.config.defaultPackage];
-      }
-    }
-    await this.config.repositoryManager.reload();
-
-
-    // Running shell
+    const shell = new Shell(this.config);
+    const { pkgNames, pkgActions, curDirPkg } = await shell.readPackages(myArgs?._);
     const osShell: OsShell = new OsShell(this.config, pkgNames, true);
     osShell.interactive = true;
+    // Execute cmd.shell and cmd.env actions supressing output
+    const originalLog = console.log;
+    const originalError = console.error;
+    try {
+      console.log = () => {};
+      console.error = () => {};
+      await shell.runShell(osShell, curDirPkg, pkgActions, false);
+
+      // code that produces output
+     } finally {
+      console.log = originalLog;
+      console.error = originalError;
+    }
     const env: Record<string, string> = {};
     await osShell.prepareEnv(env);
-    const shell = myArgs["shell"] || "cmd"
-    this.emitEnv(env, shell);
-    if (shell == "cmd" && (Deno.env.get("LEVAIN_LUA_LOADED") == undefined)) {
+    const shellTool = myArgs["shell"] || "cmd"
+    this.emitEnv(env, shellTool);
+    if (shellTool == "cmd" && (Deno.env.get("LEVAIN_LUA_LOADED") == undefined)) {
       console.log(`set "PROMPT=[%LEVAIN_CURRENT%] %_LEVAIN_OLD_PROMPT%"`);
     }
   }
